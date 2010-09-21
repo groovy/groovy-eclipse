081d12fa6c03e03501e1861569e1a6a9f7fb3e70
Andrew Eisenberg <andrew@eisenberg.as>
2010-09-21 03:01:17 +0000

    IN PROGRESS - issue GRECLIPSE-750: Type inference does not take Generic type of a List into account http://jira.codehaus.org/browse/GRECLIPSE-750
    Largely working with tests included.
    
    Still need to add content assist tests for array accesses.
    
    git-svn-id: https://svn.codehaus.org/groovy/eclipse/trunk@20792 a5544e8c-8a19-0410-ba12-f9af4593a198

--- ide/org.codehaus.groovy.eclipse.astviews/src/org/codehaus/groovy/eclipse/astviews/TreeNodeFactory.groovy
+++ ide/org.codehaus.groovy.eclipse.astviews/src/org/codehaus/groovy/eclipse/astviews/TreeNodeFactory.groovy
@@ -22,163 +22,130 @@ import org.codehaus.groovy.ast.ASTNode
  * @author emp
  */
 class TreeNodeFactory {
-	static forceDefaultNode = [
-		org.codehaus.groovy.ast.CompileUnit,
-		org.codehaus.groovy.ast.DynamicVariable,
-		org.codehaus.groovy.ast.Parameter,
-		org.codehaus.groovy.ast.Variable,
-		org.codehaus.groovy.ast.VariableScope,
-		org.codehaus.groovy.control.SourceUnit,
-	]
-	
-	static NULL_VALUE = new Object()
-	
-	static ITreeNode createTreeNode(ITreeNode parent, Object value, String displayName) {
-		if (value == null) {
-			value = NULL_VALUE
-		}
-		// The displayName attributes are set after the constructor in the code below because
-		//  displayName relies on the value object being set before it.  Since groovy does not 
-		//  honor the order of the attributes in the constructor declaration, value is null when
-		//  setDisplayName() is called and the ASTViewer becomes frustrating.
-		if (value instanceof ASTNode || forceDefaultNode.contains(value.class)) {
-			def node = new DefaultTreeNode( parent:parent, value:value )
-		    node.displayName = displayName
-		    return node
-		} else if (value instanceof Object[] || value instanceof List) {
-			def node = new CollectionTreeNode( parent:parent, value:value )
-			node.displayName = displayName
-			return node
-		} else if (value instanceof Map) {
-			def node = new MapTreeNode( parent:parent, value:value )
-			node.displayName = displayName
-			return node
-		} else {
-			def node = new AtomTreeNode( parent:parent, value:value )
-			node.displayName = displayName
-			return node
-		}
-	}
-}
+    static forceDefaultNode = [
+        org.codehaus.groovy.ast.CompileUnit,
+        org.codehaus.groovy.ast.DynamicVariable,
+        org.codehaus.groovy.ast.Parameter,
+        org.codehaus.groovy.ast.Variable,
+        org.codehaus.groovy.ast.VariableScope,
+        org.codehaus.groovy.control.SourceUnit,
+    ]
+    
+    static NULL_VALUE = new Object()
+    
+    static ITreeNode createTreeNode(ITreeNode parent, Object value, String displayName) {
+        if (value == null) {
+            value = NULL_VALUE }
+        // The displayName attributes are set after the constructor in the code below because
+        //  displayName relies on the value object being set before it.  Since groovy does not 
+        //  honor the order of the attributes in the constructor declaration, value is null when
+        //  setDisplayName() is called and the ASTViewer becomes frustrating.
+        if (value instanceof ASTNode || forceDefaultNode.contains(value.class)) {
+            def node = new DefaultTreeNode( parent:parent, value:value )
+            node.displayName = displayName
+            return node } else if (value instanceof Object[] || value instanceof List) {
+            def node = new CollectionTreeNode( parent:parent, value:value )
+            node.displayName = displayName
+            return node } else if (value instanceof Map) {
+            def node = new MapTreeNode( parent:parent, value:value )
+            node.displayName = displayName
+            return node } else {
+            def node = new AtomTreeNode( parent:parent, value:value )
+            node.displayName = displayName
+            return node } } }
 
 class StringUtil {
-	static String toString(Object o) {
-		return o.toString()
-	}
-	
-	static String toString(Class cls) {
-		// If o is Integer, then Integer.toString() tries to call either of the static toString() methods of Integer.
-		// There may be other classes with static toString(args) methods.
-		// This is the same code as in Class.toString()
+    static String toString(Object o) {
+        return o.toString() }
+    
+    static String toString(Class cls) {
+        // If o is Integer, then Integer.toString() tries to call either of the static toString() methods of Integer.
+        // There may be other classes with static toString(args) methods.
+        // This is the same code as in Class.toString()
         return (cls.isInterface() ? "interface " : (cls.isPrimitive() ? "" : "class ")) + cls.getName();
-	}
-}
+    } }
 
 abstract class TreeNode implements ITreeNode {
-	static final ITreeNode[] NO_CHILDREN = new ITreeNode[0]
-
-	ITreeNode parent
-	Object value
-	String displayName
-	Boolean leaf
-	ITreeNode[] children = null
-	
-	void setDisplayName(String name) {
-		def mappedName = MapTo.names(value)
-		if (mappedName) { name = "$name - $mappedName" }
-		displayName = name
-	}
-	
-	ITreeNode[] getChildren() {
-		if (children == null) {
-			children = loadChildren()
-			if (children == null) {
-				children = NO_CHILDREN
-			}
-		}
-		return children
-	}
-	
-	boolean isLeaf() {
-		if (leaf == null) {
-			if (children == null) {
-				children = loadChildren()
-			}
-			leaf = children.length == 0
-		}
-		return leaf
-	}
-	
-	abstract ITreeNode[] loadChildren()
-}
+    static final ITreeNode[] NO_CHILDREN = new ITreeNode[0]
+    
+    ITreeNode parent
+    Object value
+    String displayName
+    Boolean leaf
+    ITreeNode[] children = null
+    
+    void setDisplayName(String name) {
+        def mappedName = MapTo.names(value)
+        if (mappedName) { name = "$name - $mappedName" }
+        displayName = name }
+    
+    ITreeNode[] getChildren() {
+        if (children == null) {
+            children = loadChildren()
+            if (children == null) {
+                children = NO_CHILDREN } }
+        return children }
+    
+    boolean isLeaf() {
+        if (leaf == null) {
+            if (children == null) {
+                children = loadChildren() }
+            leaf = children.length == 0 }
+        return leaf }
+    
+    abstract ITreeNode[] loadChildren() }
 
 class DefaultTreeNode extends TreeNode {	
-	ITreeNode[] loadChildren() {
-		def methods = value.class.getMethods()
-		methods = methods?.findAll { it.name.startsWith('get') && it.getParameterTypes().length == 0 }
-		def children = methods?.collect { method ->
-			def name = method.name[3..-1]
-			name = name[0].toLowerCase() + name[1..-1]
-			try {
-				return TreeNodeFactory.createTreeNode(this, value."${method.name}"(), name)
-			} catch (org.codehaus.groovy.GroovyBugError e) {
-//				 Some getters are not for us.
-				return null
-			} catch (NullPointerException e) {
-				// For some reason ClassNode.getAbstractMethods() has a problem - ClassNode.superclass is null.
-				return null
-			}
-		}
-		// Make sure there are no nulls from the above exception handling.
-		children = children?.findAll { it != null }
-		if (children == null) {
-			children = NO_CHILDREN
-		}
-
-		return children
-	}
-}
+    ITreeNode[] loadChildren() {
+        def methods = value.class.getMethods()
+        methods = methods?.findAll { (it.name.startsWith('get') && it.getParameterTypes().length == 0) || it.name.equals("redirect") }
+        def children = methods?.collect { method ->
+            def name = method.name.equals("redirect") ? method.name : method.name[3..-1]
+            name = name[0].toLowerCase() + name[1..-1]
+            try {
+                return TreeNodeFactory.createTreeNode(this, value."${method.name}"(), name) } catch (org.codehaus.groovy.GroovyBugError e) {
+                //				 Some getters are not for us.
+                return null
+            } catch (NullPointerException e) {
+                // For some reason ClassNode.getAbstractMethods() has a problem - ClassNode.superclass is null.
+                return null
+            } }
+        // Make sure there are no nulls from the above exception handling.
+        children = children?.findAll { it != null }
+        if (children == null) {
+            children = NO_CHILDREN }
+        
+        return children } }
 
 // This includes object arrays.
 class CollectionTreeNode extends TreeNode {
-	ITreeNode[] loadChildren() {
-		return value.collect {
-			def name = StringUtil.toString(it)
-			if (name.indexOf('@') != -1) {
-				name = it.class.getCanonicalName()
-			}
-			TreeNodeFactory.createTreeNode(this, it, name)
-		} as ITreeNode[]
-	}
-}
+    ITreeNode[] loadChildren() {
+        return value.collect {
+            def name = StringUtil.toString(it)
+            if (name.indexOf('@') != -1) {
+                name = it.class.getCanonicalName() }
+            TreeNodeFactory.createTreeNode(this, it, name) } as ITreeNode[] } }
 
 class MapTreeNode extends TreeNode {
-	ITreeNode[] loadChildren() {
-		return value.collect { k, v -> TreeNodeFactory.createTreeNode(this, v, k) } as ITreeNode[]
-	}
-}
+    ITreeNode[] loadChildren() {
+        return value.collect { k, v -> TreeNodeFactory.createTreeNode(this, v, k) } as ITreeNode[] } }
 
 class AtomTreeNode implements ITreeNode {
-	ITreeNode parent
-	Object value
-	String displayName
-
-	boolean isLeaf() { return true }
-	
-	ITreeNode[] getChildren() { return NO_CHILDREN }
-	
-	void setDisplayName(String name) {
-		def mappedName = MapTo.names(value)
-		if (mappedName) { name = "$name - $mappedName" }
-		
-		if (value instanceof String) {
-			displayName = "$name : '${StringUtil.toString(value)}'".toString()
-		} else {
-			def valueName = StringUtil.toString(value)
-			if (valueName.indexOf('@') != -1) {
-				valueName = value.class.getCanonicalName()
-			}
-			displayName = "$name : ${StringUtil.toString(valueName)}".toString()
-		}
-	}
-}
+    ITreeNode parent
+    Object value
+    String displayName
+    
+    boolean isLeaf() { return true }
+    
+    ITreeNode[] getChildren() { return NO_CHILDREN }
+    
+    void setDisplayName(String name) {
+        def mappedName = MapTo.names(value)
+        if (mappedName) { name = "$name - $mappedName" }
+        
+        if (value instanceof String) {
+            displayName = "$name : '${StringUtil.toString(value)}'".toString() } else {
+            def valueName = StringUtil.toString(value)
+            if (valueName.indexOf('@') != -1) {
+                valueName = value.class.getCanonicalName() }
+            displayName = "$name : ${StringUtil.toString(valueName)}".toString() } } }
