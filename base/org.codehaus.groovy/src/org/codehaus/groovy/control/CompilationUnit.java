64c3a7ca5251e86f460b266f2d546a1bb06d220f
Andrew Clement <aclement@vmware.com>
2011-05-18 20:02:29 +0000

    allow groovy to be aware of a reconciling type compile
    git-svn-id: https://svn.codehaus.org/groovy/eclipse/trunk@22198 a5544e8c-8a19-0410-ba12-f9af4593a198

--- base/org.codehaus.groovy/src/org/codehaus/groovy/control/CompilationUnit.java
+++ base/org.codehaus.groovy/src/org/codehaus/groovy/control/CompilationUnit.java
@@ -1226,6 +1226,20 @@ public class CompilationUnit extends ProcessingUnit {
 		}
 		return "CompilationUnit: null";
 	}
+
+	/**
+	 * Slightly modifies the behaviour of the phases based on what the caller really needs.  Some invocations of the compilation
+	 * infrastructure don't need the bytecode, so we can skip creating it, they would rather have a more 'source like' AST.
+	 * 
+	 * @param isReconcile is this a reconciling compile?
+	 */
+	public void tweak(boolean isReconcile) {
+		if (isReconcile) {
+        	verifier.inlineStaticFieldInitializersIntoClinit=false;
+		} else {
+        	verifier.inlineStaticFieldInitializersIntoClinit=true;			
+		}
+	}
     // end
     
 }
