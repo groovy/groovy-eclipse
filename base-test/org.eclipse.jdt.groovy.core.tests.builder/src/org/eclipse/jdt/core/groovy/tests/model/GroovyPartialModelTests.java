c91396f52e20cefd044da15d4819cbd95fa837f8
Andrew Eisenberg <andrew@eisenberg.as>
2011-05-20 16:26:52 +0000

    2 failing tests for moving field initializers into constuctors.
    git-svn-id: https://svn.codehaus.org/groovy/eclipse/trunk@22214 a5544e8c-8a19-0410-ba12-f9af4593a198

--- base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/model/GroovyPartialModelTests.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/model/GroovyPartialModelTests.java
@@ -17,12 +17,21 @@ package org.eclipse.jdt.core.groovy.tests.model;
 
 import junit.framework.Test;
 
+import org.codehaus.groovy.ast.ClassCodeVisitorSupport;
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.ast.ConstructorNode;
 import org.codehaus.groovy.ast.FieldNode;
+import org.codehaus.groovy.ast.MethodNode;
+import org.codehaus.groovy.ast.Parameter;
+import org.codehaus.groovy.ast.expr.ClosureExpression;
+import org.codehaus.groovy.ast.expr.Expression;
 import org.codehaus.groovy.ast.expr.ListExpression;
+import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.jdt.groovy.model.GroovyCompilationUnit;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 
 
 /**
@@ -37,21 +46,95 @@ public class GroovyPartialModelTests  extends AbstractGroovyTypeRootTests {
         return buildTestSuite(GroovyPartialModelTests.class);
     }
     
-    // tests that a field's static initializer is not erased during a reconcile
-    public void testClassFileHasNoNonSourceChildren() throws Exception {
-        IProject project = createSimpleGroovyProject().getProject();
-        env.addGroovyClass(project.getFullPath().append("src"), "p1", "Hello2",
-                "package p1\n"+
+    // tests that a static field's initializer is not erased during a reconcile
+    public void testStaticFieldInitializerIsNotMoved1() throws Exception {
+        findFieldInitializer("package p1\n"+
                 "public class Hello {\n"+
                 "  static aStatic = []\n" +
-                "}\n"
-                );
-            
-        IFile javaFile = getFile("Project/src/p1/Hello2.groovy");
-        GroovyCompilationUnit unit = (GroovyCompilationUnit) JavaCore.createCompilationUnitFrom(javaFile);
-        FieldNode field = unit.getModuleNode().getClasses().get(0).getField("aStatic");
-        assertNotNull(field.getInitialExpression());
-        assertTrue(field.getInitialExpression() instanceof ListExpression);
+                "}\n", ListExpression.class);
     }
     
+    // tests that a static field's initializer is not erased during a reconcile
+    public void testStaticFieldInitializerIsNotMoved2() throws Exception {
+        findFieldInitializer("package p1\n"+
+                "public class Hello {\n"+
+                "  static aStatic = {}\n" +
+                "}\n", ClosureExpression.class);
+    }
+    // tests that a non-static field initializer is not erased during a reconcile
+    public void testFieldInitializerIsNotMoved1() throws Exception {
+        findFieldInitializer("package p1\n"+
+                "public class Hello {\n"+
+                "  def aStatic = []\n" +
+                "}\n", ListExpression.class);
+    }
+    
+    // tests that a non-static field initializer is not erased during a reconcile
+    public void testFieldInitializerIsNotMoved2() throws Exception {
+        findFieldInitializer("package p1\n"+
+                "public class Hello {\n"+
+                "  def aStatic = {}\n" +
+                "}\n", ClosureExpression.class);
+    }
+    
+    private Expression findFieldInitializer(String contents, Class<? extends Expression> expressionClass) throws JavaModelException {
+        IProject project = createSimpleGroovyProject().getProject();
+        env.addGroovyClass(project.getFullPath().append("src"), "p1", "Hello2", contents);
+        IFile javaFile = getFile("Project/src/p1/Hello2.groovy");
+        GroovyCompilationUnit unit = (GroovyCompilationUnit) JavaCore.createCompilationUnitFrom(javaFile);
+        ClassNode inClass = unit.getModuleNode().getClasses().get(0);
+        FieldNode field = inClass.getField("aStatic");
+        Expression initialExpression = field.getInitialExpression();
+        assertNotNull(initialExpression);
+        assertTrue(expressionClass.isInstance(initialExpression));
+        
+        checkClinitAndAllConstructors(initialExpression, inClass);
+        return initialExpression;
+    }
+    
+    
+    private void checkClinitAndAllConstructors(Expression expr, ClassNode inClass) {
+        for (ConstructorNode cons : inClass.getDeclaredConstructors()) {
+            assertUnique(expr, cons);
+        }
+        assertUnique(expr, inClass.getMethod("<clinit>", new Parameter[0]));
+    }
+    
+    // asserts that the given expression has not been copied into the constructor
+    // and so makes sure that this expression appears only once in the AST
+    private void assertUnique(Expression expr, MethodNode cons) {
+        UniquenessVisitor visitor = new UniquenessVisitor(expr);
+        visitor.visitMethod(cons);
+    }
+    // Only checks for ClosureExpressions and ListExpressions, but we can easily add more
+    class UniquenessVisitor extends ClassCodeVisitorSupport {
+        public UniquenessVisitor(Expression exprToCheck) {
+            this.exprToCheck = exprToCheck;
+        }
+
+        private final Expression exprToCheck;
+        
+        
+
+        @Override
+        protected SourceUnit getSourceUnit() {
+            return null;
+        }
+        
+        @Override
+        public void visitListExpression(ListExpression expression) {
+            doCheck(expression);
+            super.visitListExpression(expression);
+        }
+        
+        @Override
+        public void visitClosureExpression(ClosureExpression expression) {
+            doCheck(expression);
+            super.visitClosureExpression(expression);
+        }
+        
+        void doCheck(Expression expr) {
+            assertFalse ("Expression appears twice.  Once in constructor and once in field initializer.\nExpr: " + expr, expr == exprToCheck);
+        }
+    }
 }
