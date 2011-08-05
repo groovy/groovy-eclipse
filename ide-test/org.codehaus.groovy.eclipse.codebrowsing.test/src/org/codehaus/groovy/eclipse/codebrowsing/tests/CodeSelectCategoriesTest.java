7c1e846269aac01014daa4346c30e22d21670cca
Andrew Eisenberg <andrew@eisenberg.as>
2011-08-05 17:53:39 +0000

    Are test files getting updated?
    git-svn-id: https://svn.codehaus.org/groovy/eclipse/trunk@22738 a5544e8c-8a19-0410-ba12-f9af4593a198

--- ide-test/org.codehaus.groovy.eclipse.codebrowsing.test/src/org/codehaus/groovy/eclipse/codebrowsing/tests/CodeSelectCategoriesTest.java
+++ ide-test/org.codehaus.groovy.eclipse.codebrowsing.test/src/org/codehaus/groovy/eclipse/codebrowsing/tests/CodeSelectCategoriesTest.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2009 SpringSource and others.
- * All rights reserved. This program and the accompanying materials 
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Andrew Eisenberg - initial API and implementation
  *******************************************************************************/
@@ -18,7 +18,7 @@ import org.eclipse.jdt.core.IJavaElement;
 /**
  * @author Andrew Eisenberg
  * @created Aug 5, 2009
- * 
+ *
  *
  */
 public class CodeSelectCategoriesTest extends BrowsingTestCase {
@@ -26,7 +26,7 @@ public class CodeSelectCategoriesTest extends BrowsingTestCase {
     public CodeSelectCategoriesTest() {
         super(CodeSelectCategoriesTest.class.getName());
     }
-    
+
     public void testDGM() throws Exception {
         IPath projectPath = createGenericProject();
         IPath root = projectPath.append("src");
@@ -40,13 +40,13 @@ public class CodeSelectCategoriesTest extends BrowsingTestCase {
         assertEquals("Should have found a selection", 1, elt.length);
         assertEquals("Should have found local variable 'each'", "each", elt[0].getElementName());
     }
-    
+
     public void testGroovyCategory() throws Exception {
         IPath projectPath = createGenericProject();
         IPath root = projectPath.append("src");
         String contents = "class MyCategory { static doNothing(Object o) { } }";
         env.addGroovyClass(root, "", "MyCategory", contents);
-        
+
         String contents2 = "use(MyCategory) { doNothing() }";
         env.addGroovyClass(root, "", "Hello", contents2);
         incrementalBuild(projectPath);
@@ -56,5 +56,9 @@ public class CodeSelectCategoriesTest extends BrowsingTestCase {
         IJavaElement[] elt = unit.codeSelect(contents2.lastIndexOf("doNothing"), 1);
         assertEquals("Should have found a selection", 1, elt.length);
         assertEquals("Should have found local variable 'doNothing'", "doNothing", elt[0].getElementName());
+    }
+
+    public void testDummy() throws Exception {
+        fail("This dummy test should be removed");
     }
 }
