f477a3f3e069a9d723d24e250a68c35c950e3af6
Andrew Eisenberg <andrew@eisenberg.as>
2011-09-27 03:06:42 +0000

    Initial commit
    git-svn-id: https://svn.codehaus.org/groovy/eclipse/trunk@22832 a5544e8c-8a19-0410-ba12-f9af4593a198

--- extras/groovy-eclipse-quickstart/src/main/resources/archetype-resources/src/main/groovy/GroovyMain.groovy
+++ extras/groovy-eclipse-quickstart/src/main/resources/archetype-resources/src/main/groovy/GroovyMain.groovy
@@ -1,6 +1,5 @@
 class GroovyMain {
 	static void main(String... args) {
 		new GroovyHello().sayHello()
-		new JavaHello().sayHello()
 	}
 }
---
+++ extras/groovy-eclipse-quickstart/src/main/resources/archetype-resources/src/main/groovy/JavaHello.java
@@ -0,0 +1,5 @@
+class JavaHello implements Helloable {
+	public void sayHello() {
+        System.out.println("Hello, from Java.");
+	}
+}
