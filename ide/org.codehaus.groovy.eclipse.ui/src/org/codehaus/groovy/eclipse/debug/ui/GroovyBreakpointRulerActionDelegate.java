e66593dc45b22f2e83c135cb7cbab36c55986883
Andrew Eisenberg <aeisenberg@vmware.com>
2012-05-04 14:20:50 -0700

    Fix compile error when on E4.2M7 and later.

--- ide/org.codehaus.groovy.eclipse.ui/src/org/codehaus/groovy/eclipse/debug/ui/GroovyBreakpointRulerActionDelegate.java
+++ ide/org.codehaus.groovy.eclipse.ui/src/org/codehaus/groovy/eclipse/debug/ui/GroovyBreakpointRulerActionDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2002, 2009  IBM Corporation, SpringSource and others. All rights reserved. This
+ * Copyright (c) 2000-2012  IBM Corporation, SpringSource and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -14,6 +14,7 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.debug.ui.actions.RulerToggleBreakpointActionDelegate;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.source.IVerticalRulerInfo;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
@@ -23,33 +24,53 @@ import org.eclipse.ui.texteditor.ITextEditor;
 
 public class GroovyBreakpointRulerActionDelegate extends RulerToggleBreakpointActionDelegate {
 
-	private IEditorPart fEditorPart;
-	/**
-	 * @see IEditorActionDelegate#setActiveEditor(bIAction, IEditorPart)
-	 */
-	public void setActiveEditor(IAction callerAction, IEditorPart targetEditor) {
-		fEditorPart = targetEditor;
-		super.setActiveEditor(callerAction, targetEditor);
-	}
+    private IEditorPart fEditorPart;
+    private GroovyBreakpointRulerAction groovyDelegate;
+    /**
+     * @see IEditorActionDelegate#setActiveEditor(bIAction, IEditorPart)
+     */
+    @Override
+    public void setActiveEditor(IAction callerAction, IEditorPart targetEditor) {
+        fEditorPart = targetEditor;
+        super.setActiveEditor(callerAction, targetEditor);
+    }
 
-	
-	/**
-	 * @see AbstractRulerActionDelegate#createAction()
-	 */
-	protected IAction createAction(ITextEditor editor,
-			IVerticalRulerInfo rulerInfo) {
-		IResource resource;
-		IEditorInput editorInput = editor.getEditorInput();
-		if (editorInput instanceof IFileEditorInput) {
-			resource = ((IFileEditorInput) editorInput).getFile();
-			if (GroovyNature.hasGroovyNature(resource.getProject())) {
-				//it's an Groovy Project, use our action
-				return new GroovyBreakpointRulerAction(rulerInfo, editor,
-						fEditorPart);
-			}
-		}
-		//	else: use jdt's action
-		return super.createAction(editor, rulerInfo);
-	}
 
+    /**
+     * @see AbstractRulerActionDelegate#createAction()
+     */
+    @Override
+    protected IAction createAction(ITextEditor editor,
+            IVerticalRulerInfo rulerInfo) {
+        IResource resource;
+        IEditorInput editorInput = editor.getEditorInput();
+        if (editorInput instanceof IFileEditorInput) {
+            resource = ((IFileEditorInput) editorInput).getFile();
+            if (GroovyNature.hasGroovyNature(resource.getProject())) {
+                groovyDelegate = new GroovyBreakpointRulerAction(rulerInfo, editor,
+                        fEditorPart);
+                return groovyDelegate;
+            }
+        }
+        //	else: use jdt's action
+        return super.createAction(editor, rulerInfo);
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate2#runWithEvent(org.eclipse.jface.action.IAction, org.eclipse.swt.widgets.Event)
+     */
+    @Override
+    public void runWithEvent(IAction action, Event event) {
+        if(groovyDelegate != null) {
+            groovyDelegate.runWithEvent(event);
+        } else {
+            super.runWithEvent(action, event);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        groovyDelegate = null;
+        super.dispose();
+    }
 }
