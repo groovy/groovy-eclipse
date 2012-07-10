87109e924233e2b2c7611d7633fa4d508b29b0f5
Andrew Eisenberg <aeisenberg@vmware.com>
2012-07-10 09:41:58 -0700

    fix invalid merge

--- base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/search/DGMInferencingTests.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/search/DGMInferencingTests.java
@@ -339,7 +339,6 @@ public class DGMInferencingTests extends AbstractInferencingTest {
         int end = start + str.length();
         assertType(contents, start, end, "java.lang.String");
     }
-<<<<<<< HEAD
     // with groovy 2.0, there are some new DGM classes.  need to ensure that we are using those classes as the declaring type, but only for 2.0 or later
     public void testDGMDeclaring1() throws Exception {
         String contents = "\"\".eachLine";
@@ -374,42 +373,4 @@ public class DGMInferencingTests extends AbstractInferencingTest {
             assertDeclaringType(contents, start, end, "org.codehaus.groovy.runtime.DefaultGroovyMethods");
         }
     }
-=======
-    
-    // with groovy 2.0, there are some new DGM classes.  need to ensure that we are using those classes as the declaring type, but only for 2.0 or later
-    public void testDGMDeclaring1() throws Exception {
-        String contents = "\"\".eachLine";
-        String str = "eachLine";
-        int start = contents.lastIndexOf(str);
-        int end = start + str.length();
-        if (GroovyUtils.GROOVY_LEVEL >= 20) {
-            assertDeclaringType(contents, start, end, "org.codehaus.groovy.runtime.StringGroovyMethods");
-        } else {
-            assertDeclaringType(contents, start, end, "org.codehaus.groovy.runtime.DefaultGroovyMethods");
-        }
-    }
-    public void testDGMDeclaring2() throws Exception {
-        String contents = "new File().eachLine";
-        String str = "eachLine";
-        int start = contents.lastIndexOf(str);
-        int end = start + str.length();
-        if (GroovyUtils.GROOVY_LEVEL >= 20) {
-            assertDeclaringType(contents, start, end, "org.codehaus.groovy.runtime.ResourceGroovyMethods");
-        } else {
-            assertDeclaringType(contents, start, end, "org.codehaus.groovy.runtime.DefaultGroovyMethods");
-        }
-    }
-    public void testDGMDeclaring3() throws Exception {
-        String contents = "Writer w\nw.leftShift";
-        String str = "leftShift";
-        int start = contents.lastIndexOf(str);
-        int end = start + str.length();
-        if (GroovyUtils.GROOVY_LEVEL >= 20) {
-            assertDeclaringType(contents, start, end, "org.codehaus.groovy.runtime.IOGroovyMethods");
-        } else {
-            assertDeclaringType(contents, start, end, "org.codehaus.groovy.runtime.DefaultGroovyMethods");
-        }
-    }
-
->>>>>>> a bit more futzing with proeprties
 }
