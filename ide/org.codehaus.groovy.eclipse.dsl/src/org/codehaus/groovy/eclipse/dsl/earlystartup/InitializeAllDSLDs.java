d9eb9099940db6ceff7d03852768d66372e68421
Andrew Eisenberg <aeisenberg@vmware.com>
2013-04-03 16:07:22 -0700

    Fix for GRECLIPSE-1602 wait for m2e to be loaded before initializing DSLDs.

--- ide/org.codehaus.groovy.eclipse.dsl/src/org/codehaus/groovy/eclipse/dsl/earlystartup/InitializeAllDSLDs.java
+++ ide/org.codehaus.groovy.eclipse.dsl/src/org/codehaus/groovy/eclipse/dsl/earlystartup/InitializeAllDSLDs.java
@@ -19,11 +19,17 @@ import org.codehaus.groovy.eclipse.dsl.RefreshDSLDJob;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
 
 /**
  * Initializes all DSLD scripts in the workspace on startup
@@ -32,8 +38,34 @@ import org.eclipse.ui.preferences.ScopedPreferenceStore;
  * @created Nov 25, 2010
  */
 public class InitializeAllDSLDs implements IStartup {
+    final Bundle m2eJdtBundle = Platform.getBundle("org.eclipse.m2e.jdt");
+    boolean isStarted() {
+        return m2eJdtBundle == null || m2eJdtBundle.getState() == Bundle.ACTIVE;
+    }
     public void earlyStartup() {
-        initializeAll();
+        // https://jira.codehaus.org/browse/GRECLIPSE-1602
+        // check for the org.eclipse.m2e.jdt bundle and if exists, ensure started before initializing
+        
+        if (isStarted()) {
+            // m2e not installed, just go ahead
+            initializeAll();
+        } else {
+            // m2e installed, wait until bundle has been started
+            // run in a worker thread to avoid locking the early startup thread
+            new Job("Wait for m2e to start") {
+                @Override
+                protected IStatus run(IProgressMonitor monitor) {
+                    try {
+                        m2eJdtBundle.start(Bundle.START_TRANSIENT);
+                        initializeAll();
+                        return Status.OK_STATUS;
+                    } catch (BundleException e) {
+                        return new Status(IStatus.ERROR, PLUGIN_ID, "Could not start m2e plugin. See details for more information", e);
+                    }
+                }
+
+            }.schedule();
+        }
     }
 
     public void initializeAll() {
@@ -65,7 +97,7 @@ public class InitializeAllDSLDs implements IStartup {
      * Must keep this in a different method to avoid accidentally starting the DSLD plugin (and hence all of the groovy plugins).
      * @param e
      */
-    private void logException(CoreException e) {
+    private void logException(Exception e) {
         GroovyDSLCoreActivator.logException(e);
     }
     
