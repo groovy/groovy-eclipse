e0bb2ca8b778275c488c984a90d6482e35e0e77b
Andrew Eisenberg <aeisenberg@vmware.com>
2013-04-23 15:51:55 -0700

    More error checking

--- ide-test/org.codehaus.groovy.alltests/src/org/codehaus/groovy/alltests/SanityTest.java
+++ ide-test/org.codehaus.groovy.alltests/src/org/codehaus/groovy/alltests/SanityTest.java
@@ -50,8 +50,12 @@ public class SanityTest extends TestCase {
         Version groovyVersion = getGroovyCompilerVersion();
         
         System.out.println("---------------------------------------");
+        System.out.println("SanityTest.testCompilerVersion()");
         System.out.println("Using JDT version " + jdtVersion);
-        System.out.println("Usion Groovy version " + groovyVersion);
+        System.out.println("Using Groovy version " + groovyVersion);
+        System.out.println("Classloader location" + GroovyActivator.class.getClassLoader().getResource("."));
+        System.out.println("Groovy bundle status "+ (Platform.getBundle("org.codehaus.groovy").getState() == Bundle.ACTIVE ? "ACTIVE" : "NOT ACTIVE"));
+        System.out.println("Groovy bundle version "+ Platform.getBundle("org.codehaus.groovy").getVersion());
         System.out.println("---------------------------------------");
         
         // JDT 3.7 and 3.8 test against Groovy 2.0
@@ -66,7 +70,13 @@ public class SanityTest extends TestCase {
     }
     
     public void testCompilerJars() throws Exception {
+        System.out.println("---------------------------------------");
+        System.out.println("SanityTest.testCompilerJars()");
+        System.out.println("Classloader location" + GroovyActivator.class.getClassLoader().getResource("."));
         assertNotNull("Couldn't find groovy-all jar", GroovyActivator.GROOVY_ALL_JAR_URL);
         assertNotNull("Couldn't find groovy jar", GroovyActivator.GROOVY_JAR_URL);
+              }
+    
+    public static void main(String[] args) {
     }
 }
