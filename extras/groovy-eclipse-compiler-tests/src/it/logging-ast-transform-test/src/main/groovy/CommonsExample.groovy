fea16351dc200bbf63ff659be490fa9880cb7e0e
Kris De Volder <kdevolder@gopivotal.com>
2013-11-01 14:42:06 -0700

    Cleanup graven tests:  - compiled crap doesn't belong in repo  - disable lombok test (is failing)  - add verify.bsh scripts to all test projecs so they don't pass unless they produced the expected .class files

--- extras/groovy-eclipse-compiler-tests/src/it/logging-ast-transform-test/src/main/groovy/CommonsExample.groovy
+++ extras/groovy-eclipse-compiler-tests/src/it/logging-ast-transform-test/src/main/groovy/CommonsExample.groovy
@@ -13,11 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.codehaus.groovy.eclipse.maven.testing
-
 import groovy.util.logging.Commons;
 
-
 @Commons
-class Log4jExample {
+class CommonsExample {
 }
