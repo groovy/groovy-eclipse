faf48b1523e291eca5b3841f0084ba377236fc19
BoykoAlex <aboyko@gopivotal.com>
2014-05-07 14:10:41 -0400

    Init with active bundle index

--- base/org.codehaus.groovy.eclipse.compilerResolver/src/org/codehaus/groovy/frameworkadapter/util/CompilerChooser.java
+++ base/org.codehaus.groovy.eclipse.compilerResolver/src/org/codehaus/groovy/frameworkadapter/util/CompilerChooser.java
@@ -130,15 +130,18 @@ public class CompilerChooser {
                     System.out.println("Specified version not found, using " + allVersions[0] + " instead.");
                 }
             }
-//        } else {
-//            // just use highest version
-//            activeIndex = 0;
-//            // no need to uninstall unused bundles since they aren't wired
-//            for (int i = 0; i < bundles.length; i++) {
-//                Bundle bundle = bundles[i];
-//                allVersions[i] = bundle.getVersion();
-//                allSpecifiedVersions[i] = SpecifiedVersion.findVersion(bundle.getVersion());
-//            }            
+        } else {
+            // just use highest version
+            activeIndex = 0;
+            // no need to uninstall unused bundles since they aren't wired
+            for (int i = 0; i < bundles.length; i++) {
+                Bundle bundle = bundles[i];
+                allVersions[i] = bundle.getVersion();
+                allSpecifiedVersions[i] = SpecifiedVersion.findVersion(bundle.getVersion());
+                if (bundle.getState() == Bundle.ACTIVE) {
+                	activeIndex = i;
+                }
+            }            
         }
     }
     
