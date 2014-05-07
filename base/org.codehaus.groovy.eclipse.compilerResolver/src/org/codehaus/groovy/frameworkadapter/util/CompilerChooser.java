e3cccc9eb894c42968826e8940c2ec45a49ef039
BoykoAlex <aboyko@gopivotal.com>
2014-05-07 15:08:04 -0400

    Mistake correction

--- base/org.codehaus.groovy.eclipse.compilerResolver/src/org/codehaus/groovy/frameworkadapter/util/CompilerChooser.java
+++ base/org.codehaus.groovy.eclipse.compilerResolver/src/org/codehaus/groovy/frameworkadapter/util/CompilerChooser.java
@@ -134,14 +134,11 @@ public class CompilerChooser {
 //            // just use highest version
 //            activeIndex = 0;
 //            // no need to uninstall unused bundles since they aren't wired
-//            for (int i = 0; i < bundles.length; i++) {
-//                Bundle bundle = bundles[i];
-//                allVersions[i] = bundle.getVersion();
-//                allSpecifiedVersions[i] = SpecifiedVersion.findVersion(bundle.getVersion());
-//                if (bundle.getState() == Bundle.ACTIVE) {
-//                	activeIndex = i;
-//                }
-//            }            
+            for (int i = 0; i < bundles.length; i++) {
+                Bundle bundle = bundles[i];
+                allVersions[i] = bundle.getVersion();
+                allSpecifiedVersions[i] = SpecifiedVersion.findVersion(bundle.getVersion());
+            }            
         }
     }
     
