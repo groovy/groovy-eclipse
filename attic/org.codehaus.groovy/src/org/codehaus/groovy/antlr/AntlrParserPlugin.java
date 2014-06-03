d4fa5f5cc1fccf46fe03b5da2db1bbc9ed3dfd86
Vasiliy_Kizhaev <Vasiliy_Kizhaev@epam.com>
2014-06-03 16:17:45 +0400

    Revert "GRECLIPSE-1700"
    This reverts commit 47375cdaee858f3d0874548efc00fe0d6ae224a9.

--- attic/org.codehaus.groovy/src/org/codehaus/groovy/antlr/AntlrParserPlugin.java
+++ attic/org.codehaus.groovy/src/org/codehaus/groovy/antlr/AntlrParserPlugin.java
@@ -766,7 +766,6 @@ public class AntlrParserPlugin extends ASTHelper implements ParserPlugin, Groovy
         }
         classNode.addAnnotations(annotations);
         classNode.setGenericsTypes(genericsType);
-        classNode.setStaticClass((modifiers & Opcodes.ACC_STATIC) > 0);
         configureAST(classNode, classDef);
         // GRECLIPSE: start
         classNode.setNameStart(nameStart);
