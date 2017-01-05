6b6762a1f8b489dde0ed0f7dc53ec92d47dc78c9
Eric Milles <eric.milles@thomsonreuters.com>
2017-01-05 17:07:06 -0600

    Add note related to joint compilation in Ant

--- base-test/org.eclipse.jdt.groovy.core.tests.compiler/src/org/eclipse/jdt/groovy/core/tests/basic/TraitsTests.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.compiler/src/org/eclipse/jdt/groovy/core/tests/basic/TraitsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2016 the original author or authors.
+ * Copyright 2009-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1158,7 +1158,7 @@ public final class TraitsTests extends AbstractGroovyRegressionTest {
         runConformTest(sources, "c");
     }
 
-    // Java classes should be able to implement traits as well...
+    // Java classes should be able to implement traits as well -- this doesn't work in groovyc either as of Groovy 2.4.8
     public void _testTraitsInteroperability() {
         String[] sources = {
             "Sample.java",
