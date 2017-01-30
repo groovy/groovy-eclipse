279f5bd369c2473e639a73acbc75f0a0dd17d304
Eric Milles <eric.milles@thomsonreuters.com>
2017-01-30 10:37:37 -0600

    Fix for e43j8 build

--- base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/builder/BuildAccessRulesTests.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/builder/BuildAccessRulesTests.java
@@ -23,11 +23,13 @@ import junit.framework.Test;
 import org.codehaus.groovy.eclipse.core.builder.GroovyClasspathContainer;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.IAccessRule;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.tests.builder.BuilderTests;
 import org.eclipse.jdt.internal.core.ClasspathAccessRule;
+import org.osgi.framework.Version;
 
 /**
  * Tests for build path access rules (i.e. restrictions placed on classpath entries).
@@ -52,6 +54,8 @@ public final class BuildAccessRulesTests extends BuilderTests {
 
     protected IPath src;
 
+    private String problemFormat;
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
@@ -65,6 +69,14 @@ public final class BuildAccessRulesTests extends BuilderTests {
                 new IAccessRule[] {new ClasspathAccessRule(new Path("java/beans/**"), IAccessRule.K_NON_ACCESSIBLE)}, null, false) // create access restriction
         });
         env.createFolder(src);
+
+        if (Platform.getBundle("org.eclipse.jdt.core").getVersion().compareTo(Version.parseVersion("3.10")) < 0) {
+            problemFormat = "Problem : Access restriction: The type %s is not accessible due to restriction on required library ##" +
+                                            " [ resource : </Project/src/Foo.groovy> range : <%d,%d> category : <150> severity : <2>]";
+        } else {
+            problemFormat = "Problem : Access restriction: The type '%s' is not API (restriction on required library '##')" +
+                                            " [ resource : </Project/src/Foo.groovy> range : <%d,%d> category : <150> severity : <2>]";
+        }
     }
 
     private void assertAccessRestriction(String source, String... types) {
@@ -78,8 +90,7 @@ public final class BuildAccessRulesTests extends BuilderTests {
         for (String type : types) {
             int offset = -1;
             while ((offset = source.indexOf(type.trim(), offset + 1)) != -1) {
-                problems.add("Problem : Access restriction: The type '" + type.trim() + "' is not API (restriction on required library '##')" +
-                    " [ resource : </Project/src/Foo.groovy> range : <" + offset + "," + (offset + type.length()) + "> category : <150> severity : <2>]");
+                problems.add(String.format(problemFormat, type.trim(), offset, offset + type.length()));
             }
         }
 
@@ -110,7 +121,7 @@ public final class BuildAccessRulesTests extends BuilderTests {
 
     public void testAccessForExtendsGenerics() {
         String source = "import java.beans.*\n" +
-            "class Foo extends ArrayList<BeanDescriptor> {}";
+            "abstract class Foo extends ArrayList<BeanDescriptor> {}";
 
         assertAccessRestriction(source, "BeanDescriptor");
     }
