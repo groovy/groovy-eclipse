778525b1f976e58289d20369f9bf1266f74fd845
Eric Milles <eric.milles@thomsonreuters.com>
2017-04-25 03:00:07 -0500

    minor edits

--- base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/search/AllSearchTests.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/search/AllSearchTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2016 the original author or authors.
+ * Copyright 2009-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,11 +18,7 @@ package org.eclipse.jdt.core.groovy.tests.search;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-/**
- * @author Andrew Eisenberg
- * @created Oct 22, 2009
- */
-public class AllSearchTests {
+public final class AllSearchTests {
     public static Test suite() throws Exception {
         TestSuite suite = new TestSuite(AllSearchTests.class.getName());
         suite.addTestSuite(BinarySearchTests.class);
