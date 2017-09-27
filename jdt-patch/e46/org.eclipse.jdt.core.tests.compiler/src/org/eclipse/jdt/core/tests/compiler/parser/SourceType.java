ce6c51750dd2e8182f1eee33831bff62cbd6487c
Eric Milles <eric.milles@thomsonreuters.com>
2017-09-27 15:28:38 -0500

    Add JDT core tests for Eclipse Neon

---
+++ jdt-patch/e46/org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/compiler/parser/SourceType.java
@@ -0,0 +1,293 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.core.tests.compiler.parser;
+
+import org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants;
+
+public final class SourceType {
+	private int modifiers;
+	private int declarationStart;
+	private int declarationEnd;
+	private char[] fileName;
+	private SourcePackage packageName;
+	private SourceImport[] imports;
+	private char[] enclosingTypeName;
+	private char[] name;
+	private int nameSourceStart;
+	private int nameSourceEnd;
+	private char[] superclassName;
+	private char[][] interfaceNames;
+	private SourceType[] memberTypes;
+	private int numberOfMemberTypes;
+	private SourceMethod[] methods;
+	private int numberOfMethods;
+	private SourceField[] fields;
+	private int numberOfFields;
+	private char[] source;
+	SourceType parent;
+	char[][] typeParameterNames;
+	char[][][] typeParameterBounds;
+
+	// Buffering.
+	private char[] qualifiedName;
+	private String defaultConstructor;
+public SourceType(
+	char[] enclosingTypeName,
+	int declarationStart,
+	int modifiers,
+	char[] name,
+	int nameSourceStart,
+	int nameSourceEnd,
+	char[] superclassName,
+	char[][] interfaceNames,
+	char[] source) {
+
+	this.enclosingTypeName = enclosingTypeName;
+	this.declarationStart = declarationStart;
+
+	this.modifiers = modifiers;
+	this.name = name;
+	this.nameSourceStart = nameSourceStart;
+	this.nameSourceEnd = nameSourceEnd;
+	this.superclassName = superclassName;
+	this.interfaceNames = interfaceNames;
+	this.source = source;
+}
+protected void addField(SourceField sourceField) {
+	if (this.fields == null) {
+		this.fields = new SourceField[4];
+	}
+
+	if (this.numberOfFields == this.fields.length) {
+		System.arraycopy(
+			this.fields,
+			0,
+			this.fields = new SourceField[this.numberOfFields * 2],
+			0,
+			this.numberOfFields);
+	}
+	this.fields[this.numberOfFields++] = sourceField;
+}
+protected void addMemberType(SourceType sourceMemberType) {
+	if(this.memberTypes == null) {
+		this.memberTypes = new SourceType[4];
+	}
+
+	if(this.numberOfMemberTypes == this.memberTypes.length) {
+		System.arraycopy(this.memberTypes, 0, this.memberTypes = new SourceType[this.numberOfMemberTypes * 2], 0, this.numberOfMemberTypes);
+	}
+	this.memberTypes[this.numberOfMemberTypes++] = sourceMemberType;
+}
+protected void addMethod(SourceMethod sourceMethod) {
+	if (this.methods == null) {
+		this.methods = new SourceMethod[4];
+	}
+
+	if (this.numberOfMethods == this.methods.length) {
+		System.arraycopy(
+			this.methods,
+			0,
+			this.methods = new SourceMethod[this.numberOfMethods * 2],
+			0,
+			this.numberOfMethods);
+	}
+	this.methods[this.numberOfMethods++] = sourceMethod;
+}
+public String displayModifiers() {
+	StringBuffer buffer = new StringBuffer();
+
+	if (this.modifiers == 0)
+		return null;
+	if ((this.modifiers & ClassFileConstants.AccPublic) != 0)
+		buffer.append("public ");
+	if ((this.modifiers & ClassFileConstants.AccProtected) != 0)
+		buffer.append("protected ");
+	if ((this.modifiers & ClassFileConstants.AccPrivate) != 0)
+		buffer.append("private ");
+	if ((this.modifiers & ClassFileConstants.AccFinal) != 0)
+		buffer.append("final ");
+	if ((this.modifiers & ClassFileConstants.AccStatic) != 0)
+		buffer.append("static ");
+	if ((this.modifiers & ClassFileConstants.AccAbstract) != 0)
+		buffer.append("abstract ");
+	if ((this.modifiers & ClassFileConstants.AccNative) != 0)
+		buffer.append("native ");
+	if ((this.modifiers & ClassFileConstants.AccSynchronized) != 0)
+		buffer.append("synchronized ");
+	return buffer.toString().trim();
+}
+public String getActualName() {
+	StringBuffer buffer = new StringBuffer();
+	buffer.append(this.source, this.nameSourceStart, this.nameSourceEnd - this.nameSourceStart + 1);
+	return buffer.toString();
+}
+public int getDeclarationSourceEnd() {
+	return this.declarationEnd;
+}
+public int getDeclarationSourceStart() {
+	return this.declarationStart;
+}
+public char[] getEnclosingTypeName() {
+	return this.enclosingTypeName;
+}
+public SourceField[] getFields() {
+	if (this.fields != null && this.fields.length != this.numberOfFields) {
+		System.arraycopy(this.fields, 0, this.fields = new SourceField[this.numberOfFields], 0, this.numberOfFields);
+	}
+	return this.fields;
+}
+public char[] getFileName() {
+	return this.fileName;
+}
+public char[][] getImports() {
+	if (this.imports == null) return null;
+	int importLength = this.imports.length;
+	char[][] importNames = new char[importLength][];
+	for (int i = 0, max = importLength; i < max; i++) {
+		importNames[i] = this.imports[i].name;
+	}
+	return importNames;
+}
+public char[][] getInterfaceNames() {
+	return this.interfaceNames;
+}
+public SourceType[] getMemberTypes() {
+	if (this.memberTypes != null && this.memberTypes.length != this.numberOfMemberTypes) {
+		System.arraycopy(
+			this.memberTypes,
+			0,
+			this.memberTypes = new SourceType[this.numberOfMemberTypes],
+			0,
+			this.numberOfMemberTypes);
+	}
+	return this.memberTypes;
+}
+public SourceMethod[] getMethods() {
+	if (this.methods != null && this.methods.length != this.numberOfMethods) {
+		System.arraycopy(this.methods, 0, this.methods = new SourceMethod[this.numberOfMethods], 0, this.numberOfMethods);
+	}
+	return this.methods;
+}
+public int getModifiers() {
+	return this.modifiers;
+}
+public char[] getName() {
+	return this.name;
+}
+public int getNameSourceEnd() {
+	return this.nameSourceEnd;
+}
+public int getNameSourceStart() {
+	return this.nameSourceStart;
+}
+public char[] getPackageName() {
+	return this.packageName.name;
+}
+public char[] getQualifiedName() {
+	if (this.qualifiedName == null) {
+		StringBuffer temp = new StringBuffer();
+		temp.append(this.packageName);
+		temp.append('.');
+		temp.append(this.name);
+		this.qualifiedName = temp.toString().toCharArray();
+	}
+	return this.qualifiedName;
+}
+public char[] getSuperclassName() {
+	return this.superclassName;
+}
+public boolean isBinaryType() {
+	return false;
+}
+public boolean isClass() {
+	return (this.modifiers & ClassFileConstants.AccInterface) == 0;
+}
+public boolean isInterface() {
+	return (this.modifiers & ClassFileConstants.AccInterface) == ClassFileConstants.AccInterface;
+}
+public void setDeclarationSourceEnd(int position) {
+	this.declarationEnd = position;
+}
+public void setDefaultConstructor(String s) {
+	this.defaultConstructor = s;
+}
+public void setImports(SourceImport[] imports) {
+	this.imports = imports;
+}
+public void setPackage(SourcePackage sourcePackage) {
+	this.packageName = sourcePackage;
+}
+public void setSuperclass(char[] superclassName) {
+	this.superclassName = superclassName;
+}
+public void setSuperinterfaces(char[][] superinterfacesNames) {
+	this.interfaceNames = superinterfacesNames;
+}
+public String tabString(int tab) {
+	/*slow code*/
+
+	String s = "";
+	for (int i = tab; i > 0; i--)
+		s = s + "\t";
+	return s;
+}
+public String toString() {
+	return toString(0);
+}
+public String toString(int tab) {
+
+	StringBuffer buffer = new StringBuffer();
+	if (this.packageName != null) {
+		buffer.append(tabString(tab)).append(this.packageName);
+	}
+	if (this.imports != null) {
+		for (int i = 0, max = this.imports.length; i < max; i++) {
+			buffer.append(tabString(tab)).append(this.imports[i]);
+		}
+	}
+	buffer.append(tabString(tab));
+	String displayModifiers = displayModifiers();
+	if (displayModifiers != null) {
+		buffer.append(displayModifiers).append(" ");
+	}
+	buffer.append(isInterface() ? "interface " : "class ").append(this.name).append(" ");
+	if (this.superclassName != null) {
+		buffer.append("extends ").append(this.superclassName).append(" ");
+	}
+	if (this.interfaceNames != null) {
+		buffer.append("implements ");
+		for (int i = 0, max = this.interfaceNames.length; i < max; i++) {
+			buffer.append(this.interfaceNames[i]).append(", ");
+		}
+	}
+	buffer.append("{\n");
+	if (this.memberTypes != null) {
+		for (int i = 0, max = this.numberOfMemberTypes; i < max; i++) {
+			buffer.append(this.memberTypes[i].toString(tab + 1)).append("\n");
+		}
+	}
+	if (this.fields != null) {
+		for (int i = 0, max = this.numberOfFields; i < max; i++) {
+			buffer.append(this.fields[i].toString(tab + 1)).append("\n");
+		}
+	}
+	if (this.defaultConstructor != null) {
+			buffer.append(tabString(tab + 1)).append(this.defaultConstructor);
+	}
+	if (this.methods != null) {
+		for (int i = 0, max = this.numberOfMethods; i < max; i++) {
+			buffer.append(this.methods[i].toString(tab + 1)).append("\n");
+		}
+	}
+	buffer.append(tabString(tab)).append("}");
+	return buffer.toString();
+}
+}
---
+++ jdt-patch/e46/org.eclipse.jdt.core.tests.compiler/src/org/eclipse/jdt/core/tests/runtime/TargetInterface.java
@@ -0,0 +1,193 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.core.tests.runtime;
+
+import org.eclipse.jdt.internal.compiler.ClassFile;
+import org.eclipse.jdt.internal.compiler.util.Util;
+
+import java.io.*;
+import java.net.*;
+/**
+ * This is the interface to the target VM. It connects to an IDEInterface on the target side
+ * using TCP/IO to send request for code snippet evaluation and to get the result back.
+ *
+ * @see org.eclipse.jdt.core.tests.eval.target.IDEInterface for details about the protocol.
+ */
+public class TargetInterface {
+	/**
+	 * Whether class files should be written in d:\eval\ instead of sending them to the target
+	 * NB: d:\eval should contain a batch file TestCodeSnippet.bat with the following contents:
+	 *		SET JDK=c:\jdk1.2.2
+	 *		SET EVAL=d:\eval
+	 *		%JDK%\bin\java -Xbootclasspath:%JDK%\jre\lib\rt.jar;%EVAL%\javaClasses; -classpath c:\temp;%EVAL%\snippets;%EVAL%\classes;"d:\ide\project_resources\Eclipse Java Evaluation\CodeSnippetSupport.jar" CodeSnippetTester %1
+	 */
+	static final boolean DEBUG = false;
+	String codeSnippetClassName;
+
+	/**
+	 * Whether timing info should be printed to stdout
+	 */
+	static final boolean TIMING = false;
+	long sentTime;
+
+	/**
+	 * The connection to the target's ide interface.
+	 */
+	Socket socket;
+
+	public class Result {
+		public char[] displayString;
+		public char[] typeName;
+	}
+
+/**
+ * (PRIVATE API)
+ * Connects this interface to the target.
+ * Try as long as the given time (in ms) has not expired.
+ * Use isConnected() to find out if the connection was successful.
+ */
+public void connect(String targetAddress, int portNumber, int timeout) {
+	if (isConnected()) {
+		return;
+	}
+	if (portNumber > 0) {
+		long startTime = System.currentTimeMillis();
+		do {
+			try {
+				this.socket = new Socket(targetAddress, portNumber);
+				this.socket.setTcpNoDelay(true);
+				break;
+			} catch (UnknownHostException e) {
+			} catch (IOException e) {
+			}
+			if (this.socket == null) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+				}
+			}
+		} while (!isConnected() && (System.currentTimeMillis() - startTime) < timeout);
+	}
+}
+/**
+ * (PRIVATE API)
+ * Disconnects this interface from the target.
+ */
+public void disconnect() {
+	if (this.socket != null) {
+		try {
+			this.socket.close();
+		} catch (IOException e) {
+			// Already closed. Nothing more to do
+		}
+		this.socket = null;
+	}
+}
+/**
+ * Returns the result of the evaluation sent previously to the target.
+ */
+public Result getResult() {
+	boolean hasValue = false;
+	String typeName = null;
+	String toString = null;
+	if (DEBUG) {
+		hasValue = true;
+		typeName = "TargetInterface in debug mode. Run d:\\eval\\TestCodeSnippet.bat d:\\eval\\snippets\\" + this.codeSnippetClassName;
+		toString = "";
+	} else {
+		if (isConnected()) {
+			// TBD: Read type name and toString as a character array
+			try {
+				DataInputStream in = new DataInputStream(this.socket.getInputStream());
+				hasValue = in.readBoolean();
+				if (hasValue) {
+					typeName = in.readUTF();
+					toString = in.readUTF();
+				} else {
+					typeName = null;
+					toString = null;
+				}
+			} catch (IOException e) {
+				// The socket has likely been closed on the other end. So the code snippet runner has stopped.
+				hasValue = true;
+				typeName = e.getMessage();
+				toString = "";
+				disconnect();
+			}
+		} else {
+			hasValue = true;
+			typeName = "Connection has been lost";
+			toString = "";
+		}
+	}
+	if (TIMING) {
+		System.out.println("Time to send compiled classes, run on target and get result is " + (System.currentTimeMillis() - this.sentTime) + "ms");
+	}
+	Result result = new Result();
+	result.displayString = toString == null ? null : toString.toCharArray();
+	result.typeName = typeName == null ? null : typeName.toCharArray();
+	return result;
+}
+/**
+ * Returns whether this interface is connected to the target.
+ */
+boolean isConnected() {
+	return this.socket != null;
+}
+/**
+ * Sends the given class definitions to the target for loading and (if specified) for running.
+ */
+public void sendClasses(boolean mustRun, ClassFile[] classes) throws TargetException {
+	if (DEBUG) {
+		for (int i = 0; i < classes.length; i++) {
+			String className = new String(classes[i].fileName()).replace('/', '\\') + ".class";
+			if ((i == 0) && (className.indexOf("CodeSnippet") != -1)) {
+				this.codeSnippetClassName = className;
+				try {
+					Util.writeToDisk(true, "d:\\eval\\snippets", className, classes[0]);
+				} catch(IOException e) {
+				}
+			} else {
+				String dirName;
+				if (className.startsWith("java")) {
+					dirName = "d:\\eval\\" + LocalVMLauncher.BOOT_CLASSPATH_DIRECTORY;
+				} else {
+					dirName = "d:\\eval\\" + LocalVMLauncher.REGULAR_CLASSPATH_DIRECTORY;
+				}
+				try {
+					Util.writeToDisk(true, dirName, className, classes[i]);
+				} catch(IOException e) {
+				}
+			}
+		}
+	} else {
+		if (TIMING) {
+			this.sentTime = System.currentTimeMillis();
+		}
+		if (!isConnected()) {
+			throw new TargetException("Connection to the target VM has been lost");
+		}
+		try {
+			DataOutputStream out = new DataOutputStream(this.socket.getOutputStream());
+			out.writeBoolean(mustRun);
+			out.writeInt(classes.length);
+			for (int i = 0; i < classes.length; i++) {
+				byte[] classDefinition = classes[i].getBytes();
+				out.writeInt(classDefinition.length);
+				out.write(classDefinition);
+			}
+		} catch (IOException e) {
+			// The socket has likely been closed on the other end. So the code snippet runner has stopped.
+			disconnect();
+		}
+	}
+}
+}
