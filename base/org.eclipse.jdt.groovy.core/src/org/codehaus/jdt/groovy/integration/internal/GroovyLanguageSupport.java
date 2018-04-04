07668b45d804b375cc43a4683e2c6c0416954880
Eric Milles <eric.milles@thomsonreuters.com>
2018-04-04 11:02:08 -0500

    Use constant

--- base/org.eclipse.jdt.groovy.core/src/org/codehaus/jdt/groovy/integration/internal/GroovyLanguageSupport.java
+++ base/org.eclipse.jdt.groovy.core/src/org/codehaus/jdt/groovy/integration/internal/GroovyLanguageSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2017 the original author or authors.
+ * Copyright 2009-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -202,7 +202,7 @@ public class GroovyLanguageSupport implements LanguageSupport {
         }
 
         if ((compilerOptions.groovyFlags & CompilerUtils.InvokeDynamic) != 0) {
-            config.getOptimizationOptions().put(/*CompilerConfiguration.INVOKEDYNAMIC*/"indy", Boolean.TRUE);
+            config.getOptimizationOptions().put(CompilerConfiguration.INVOKEDYNAMIC, Boolean.TRUE);
         }
 
         return config;
