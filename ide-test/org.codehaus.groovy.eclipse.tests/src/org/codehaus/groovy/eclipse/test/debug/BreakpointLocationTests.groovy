d7029fcbb76a34234bc3e665cf57d7bec3115124
Eric Milles <eric.milles@thomsonreuters.com>
2018-04-13 10:57:31 -0500

    Refactor breakpoint tests

--- ide-test/org.codehaus.groovy.eclipse.tests/src/org/codehaus/groovy/eclipse/test/debug/BreakpointLocationTests.groovy
+++ ide-test/org.codehaus.groovy.eclipse.tests/src/org/codehaus/groovy/eclipse/test/debug/BreakpointLocationTests.groovy
@@ -16,136 +16,361 @@
 package org.codehaus.groovy.eclipse.test.debug
 
 import org.codehaus.groovy.ast.ASTNode
+import org.codehaus.groovy.ast.ConstructorNode
+import org.codehaus.groovy.ast.MethodNode
 import org.codehaus.groovy.eclipse.debug.ui.BreakpointLocationFinder
+import org.codehaus.groovy.eclipse.test.GroovyEclipseTestSuite
+import org.codehaus.jdt.groovy.model.GroovyCompilationUnit
 import org.eclipse.jface.text.Document
 import org.junit.Test
 
-/**
- * Tests that breakpoint locations are as expected.
- */
-final class BreakpointLocationTests extends BreakpointTestSuite {
+final class BreakpointLocationTests extends GroovyEclipseTestSuite {
 
-    private void doBreakpointTest(int i) {
-        def document = new Document(String.valueOf(unit.contents))
-        int location = document.get().indexOf('// ' + i) - 3
-        int line = document.getLineOfOffset(location) + 1
+    private ASTNode findBreakpointLocation(String target, CharSequence source, String sourcePackage = '') {
+        GroovyCompilationUnit unit = addGroovySource(source, nextUnitName(), sourcePackage)
+        Document document = new Document(String.valueOf(unit.contents))
+        int offset = document.get().indexOf(target)
+        int line = document.getLineOfOffset(offset)
 
-        ASTNode node = new BreakpointLocationFinder(unit.moduleNode).findBreakpointLocation(line)
-        assert node?.lineNumber == line
+        def finder = new BreakpointLocationFinder(unit.moduleNode)
+        return finder.findBreakpointLocation(line + 1)
     }
+
+    //--------------------------------------------------------------------------
 
     @Test
     void testBreakpointInScript1() {
-        doBreakpointTest(1)
+        def node = findBreakpointLocation 'comment', '''\
+            // some comment text
+            def t = [ x:1, y:2 ]
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
     void testBreakpointInScript2() {
-        doBreakpointTest(2)
+        def node = findBreakpointLocation 'def t', '''\
+            // some comment text
+            def t = [ x:1, y:2 ]
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
     void testBreakpointInScript3() {
-        doBreakpointTest(3)
+        def node = findBreakpointLocation 't.getX', '''\
+            def t = [ x:1, y:2 ]
+            t.getX()
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
     void testBreakpointInScript4() {
-        doBreakpointTest(4)
+        def node = findBreakpointLocation 'it.x', '''\
+            def shiftTriangle = { it ->
+                it.x += 1
+                it.y += 1
+                it.getX()
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
     void testBreakpointInScript5() {
-        doBreakpointTest(5)
+        def node = findBreakpointLocation 'it.y', '''\
+            def shiftTriangle = { it ->
+                it.x += 1
+                it.y += 1
+                it.getX()
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 3
     }
 
     @Test
     void testBreakpointInScript6() {
-        doBreakpointTest(6)
+        def node = findBreakpointLocation 'it.getX', '''\
+            def shiftTriangle = { it ->
+                it.x += 1
+                it.y += 1
+                it.getX()
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 4
     }
 
     @Test
     void testBreakpointInScript7() {
-        doBreakpointTest(7)
+        def node = findBreakpointLocation 'y:', '''\
+            def t
+            t = [ x: 1,
+                  y: 2,
+                  z: 4 ]
+            '''.stripIndent()
+
+        assert node?.lineNumber == 3
     }
 
     @Test
     void testBreakpointInScript8() {
-        doBreakpointTest(8)
+        def node = findBreakpointLocation 'z:', '''\
+            def t
+            t = [ x: 1,
+                  y: 2,
+                  z: 4 ]
+            '''.stripIndent()
+
+        assert node?.lineNumber == 4
     }
 
     @Test
     void testBreakpointInScript9() {
-        doBreakpointTest(9)
+        def node = findBreakpointLocation '1', '''\
+            def t
+            t = [ 1,
+                  2,
+                  3]
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
     void testBreakpointInScript10() {
-        doBreakpointTest(10)
+        def node = findBreakpointLocation '2', '''\
+            def t
+            t = [ 1,
+                  2,
+                  3]
+            '''.stripIndent()
+
+        assert node?.lineNumber == 3
     }
 
     @Test
     void testBreakpointInScript11() {
-        doBreakpointTest(11)
+        def node = findBreakpointLocation '3', '''\
+            def t
+            t = [ 1,
+                  2,
+                  3]
+            '''.stripIndent()
+
+        assert node?.lineNumber == 4
     }
 
     @Test
     void testBreakpointInScript12() {
-        doBreakpointTest(12)
+        def node = findBreakpointLocation '[]', '''\
+            def t
+            t = [];
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
     void testBreakpointInScript13() {
-        doBreakpointTest(13)
+        def node = findBreakpointLocation 'def x', '''\
+            def x() {
+                print "Hi"
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 1
     }
 
     @Test
     void testBreakpointInScript14() {
-        doBreakpointTest(14)
+        def node = findBreakpointLocation 'print', '''\
+            def x() {
+                print "Hi"
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
     void testBreakpointInScript15() {
-        doBreakpointTest(15)
+        def node = findBreakpointLocation 'print', '''\
+            def p = { g -> print g }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 1
+    }
+
+    //
+
+    @Test
+    void testBreakpointInClass1() {
+        def node = findBreakpointLocation 'Class', '''\
+            class Class {
+              Class() {
+                super()
+              }
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
-    void testBreakpointInScript16() {
-        doBreakpointTest(16)
+    void testBreakpointInClass2() {
+        def node = findBreakpointLocation 'Class()', '''\
+            class Class {
+              Class() {
+                super()
+              }
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
-    void testBreakpointInScript17() {
-        doBreakpointTest(17)
+    void testBreakpointInClass3() {
+        def node = findBreakpointLocation 'super()', '''\
+            class Class {
+              Class() {
+                super()
+              }
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 3
     }
 
     @Test
-    void testBreakpointInScript18() {
-        doBreakpointTest(18)
+    void testBreakpointInClass4() {
+        def node = findBreakpointLocation '}', '''\
+            class Class {
+              Class() {
+                super()
+              }
+            }
+            '''.stripIndent()
+
+        assert node == null
     }
 
     @Test
-    void testBreakpointInScript19() {
-        doBreakpointTest(19)
+    void testBreakpointInClass5() {
+        def node = findBreakpointLocation 'here()', '''\
+            class Class {
+              static {
+                here()
+              }
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 3
     }
 
     @Test
-    void testBreakpointInScript20() {
-        doBreakpointTest(20)
+    void testBreakpointInClass6() {
+        def node = findBreakpointLocation 'm()', '''\
+            class Class {
+              def m() {
+                here()
+              }
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
-    void testBreakpointInScript21() {
-        doBreakpointTest(21)
+    void testBreakpointInClass7() {
+        def node = findBreakpointLocation 'here()', '''\
+            class Class {
+              def m() {
+                here()
+              }
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 3
     }
 
     @Test
-    void testBreakpointInScript22() {
-        doBreakpointTest(22)
+    void testBreakpointInClass8() {
+        def node = findBreakpointLocation 'here()', '''\
+            class Class {
+              def t = { here() }
+            }
+            '''.stripIndent()
+
+        assert node?.lineNumber == 2
     }
 
     @Test
-    void testBreakpointInScript23() {
-        doBreakpointTest(23)
+    void testBreakpointInClass9() {
+        def node = findBreakpointLocation 'property', '''\
+            class Class {
+              String property
+            }
+            '''.stripIndent()
+
+        assert node == null
+    }
+
+    @Test
+    void testBreakpointInClass10() {
+        def node = findBreakpointLocation '*', '''\
+            /**
+             *
+             */
+            class Class {
+              Class() {
+                super()
+              }
+            }
+            '''.stripIndent()
+
+        assert node instanceof ConstructorNode
+        assert node.lineNumber == 5
+    }
+
+    @Test
+    void testBreakpointInClass11() {
+        def node = findBreakpointLocation '*', '''\
+            class Class {
+              /**
+               *
+               */
+              Class() {
+                super()
+              }
+            }
+            '''.stripIndent()
+
+        assert node instanceof ConstructorNode
+        assert node.lineNumber == 5
+    }
+
+    @Test
+    void testBreakpointInClass12() {
+        def node = findBreakpointLocation '*', '''\
+            class Class {
+              /**
+               *
+               */
+              def m() {
+                here()
+              }
+            }
+            '''.stripIndent()
+
+        assert node instanceof MethodNode
+        assert node.lineNumber == 5
     }
 }
