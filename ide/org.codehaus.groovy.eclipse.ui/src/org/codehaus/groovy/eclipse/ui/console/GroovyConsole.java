5108df95e3eeb126bc90844975e1e56dc73f215f
Eric Milles <eric.milles@thomsonreuters.com>
2018-09-20 17:02:40 -0500

    Fix for #666: support themes by loading text colors from color registry

--- ide/org.codehaus.groovy.eclipse.ui/src/org/codehaus/groovy/eclipse/ui/console/GroovyConsole.java
+++ ide/org.codehaus.groovy.eclipse.ui/src/org/codehaus/groovy/eclipse/ui/console/GroovyConsole.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2017 the original author or authors.
+ * Copyright 2009-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,12 @@
  */
 package org.codehaus.groovy.eclipse.ui.console;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.codehaus.groovy.eclipse.TraceCategory;
 import org.eclipse.debug.ui.IDebugUIConstants;
-import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.preference.JFacePreferences;
+import org.eclipse.jface.resource.ColorRegistry;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.text.ITypedRegion;
 import org.eclipse.jface.text.rules.FastPartitioner;
@@ -31,33 +31,68 @@ import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
-import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.console.IConsoleDocumentPartitioner;
 import org.eclipse.ui.console.IConsoleView;
 import org.eclipse.ui.console.TextConsole;
 import org.eclipse.ui.part.IPageBookViewPage;
 
-
 public class GroovyConsole extends TextConsole {
 
-    public final static String CONSOLE_TYPE = "GroovyEventTraceConsole"; //$NON-NLS-1$
+    public static final String CONSOLE_TYPE = "GroovyEventTraceConsole"; //$NON-NLS-1$
 
-    final static RuleBasedPartitionScanner scanner = new RuleBasedPartitionScanner();
-    {
-        List<IPredicateRule> rules = new ArrayList<>(TraceCategory.values().length);
-        for (TraceCategory category : TraceCategory.values()) {
-            rules.add(new SingleLineRule(category.getPaddedLabel(), "", new Token(category.label)));
-        }
-        scanner.setPredicateRules(rules.toArray(new IPredicateRule[0]));
+    private static final RuleBasedPartitionScanner scanner = new RuleBasedPartitionScanner();
+    static {
+        scanner.setPredicateRules(Arrays.stream(TraceCategory.values()).map(category ->
+            new SingleLineRule(category.getPaddedLabel(), "", new Token(category.label))
+        ).toArray(IPredicateRule[]::new));
     }
+
+    private IConsoleDocumentPartitioner partitioner = new GroovyEventTraceConsolePartitioner();
+
+    private IPropertyChangeListener propertyListener = event -> {
+        String property = event.getProperty();
+        if (property.equals(IDebugUIConstants.PREF_CONSOLE_FONT)) {
+            setFont(JFaceResources.getFont(IDebugUIConstants.PREF_CONSOLE_FONT));
+        }
+    };
+
+    public GroovyConsole() {
+        super("Groovy Event Trace Console", CONSOLE_TYPE, null, true);
+        setFont(JFaceResources.getFont(IDebugUIConstants.PREF_CONSOLE_FONT));
+        partitioner.connect(getDocument());
+    }
+
+    @Override
+    protected void init() {
+        JFaceResources.getFontRegistry().addListener(propertyListener);
+    }
+
+    @Override
+    protected void dispose() {
+        JFaceResources.getFontRegistry().removeListener(propertyListener);
+        super.dispose();
+    }
+
+    @Override
+    public IPageBookViewPage createPage(IConsoleView view) {
+        return new GroovyConsolePage(this, view);
+    }
+
+    @Override
+    protected IConsoleDocumentPartitioner getPartitioner() {
+        return partitioner;
+    }
+
+    //--------------------------------------------------------------------------
 
     /**
      * Provides a partitioner for this console type
      */
-    class GroovyEventTraceConsolePartitioner extends FastPartitioner implements IConsoleDocumentPartitioner {
+    private class GroovyEventTraceConsolePartitioner extends FastPartitioner implements IConsoleDocumentPartitioner {
 
-        public GroovyEventTraceConsolePartitioner() {
+        GroovyEventTraceConsolePartitioner() {
             super(scanner, TraceCategory.stringValues());
             getDocument().setDocumentPartitioner(this);
         }
@@ -69,75 +104,24 @@ public class GroovyConsole extends TextConsole {
 
         @Override
         public StyleRange[] getStyleRanges(int offset, int length) {
-            ITypedRegion regions[] = computePartitioning(offset, length);
-            StyleRange[] styles = new StyleRange[regions.length];
-            for (int i = 0; i < regions.length; i++) {
-                if (TraceCategory.CLASSPATH.label.equals(regions[i].getType())) {
-                    styles[i] = new StyleRange(offset, length, Display.getDefault().getSystemColor(SWT.COLOR_DARK_GREEN), null);
-                } else if (TraceCategory.COMPILER.label.equals(regions[i].getType())) {
-                    styles[i] = new StyleRange(offset, length, Display.getDefault().getSystemColor(SWT.COLOR_BLUE), null);
-                } else if (TraceCategory.DSL.label.equals(regions[i].getType())) {
-                    styles[i] = new StyleRange(offset, length, Display.getDefault().getSystemColor(SWT.COLOR_DARK_RED), null);
-                } else if (TraceCategory.REFACTORING.label.equals(regions[i].getType())) {
-                    styles[i] = new StyleRange(offset, length, Display.getDefault().getSystemColor(SWT.COLOR_DARK_YELLOW), null);
+            ColorRegistry registry = JFaceResources.getColorRegistry();
+            return Arrays.stream(computePartitioning(offset, length)).map(ITypedRegion::getType).map(type -> {
+                Color color;
+                if (type.equals(TraceCategory.CLASSPATH.label)) {
+                    color = registry.get(JFacePreferences.ERROR_COLOR);
+                } else if (type.equals(TraceCategory.COMPILER.label) ||
+                        type.equals(TraceCategory.AST_TRANSFORM.label)) {
+                    color = registry.get(JFacePreferences.COUNTER_COLOR);
+                } else if (type.equals(TraceCategory.CODE_SELECT.label) ||
+                        type.equals(TraceCategory.CONTENT_ASSIST.label)) {
+                    color = registry.get(JFacePreferences.DECORATIONS_COLOR);
+                } else if (type.equals(TraceCategory.DSL.label)) {
+                    color = Display.getDefault().getSystemColor(SWT.COLOR_MAGENTA);
                 } else {
-                    styles[i] = new StyleRange(offset, length, null, null);
+                    color = registry.get(JFacePreferences.INFORMATION_FOREGROUND_COLOR);
                 }
-            }
-            return styles;
+                return new StyleRange(offset, length, color, null);
+            }).toArray(StyleRange[]::new);
         }
-    }
-
-    public GroovyConsole(String name, String consoleType, ImageDescriptor imageDescriptor, boolean autoLifecycle) {
-        super(name, consoleType, imageDescriptor, autoLifecycle);
-    }
-
-    private GroovyEventTraceConsolePartitioner partitioner = new GroovyEventTraceConsolePartitioner();
-
-    private IPropertyChangeListener propertyListener = event -> {
-        String property = event.getProperty();
-        if (property.equals(IDebugUIConstants.PREF_CONSOLE_FONT)) {
-            setFont(JFaceResources.getFont(IDebugUIConstants.PREF_CONSOLE_FONT));
-        }
-    };
-
-    public GroovyConsole() {
-        super("Groovy Event Trace Console", CONSOLE_TYPE, null, true);
-        Font font = JFaceResources.getFont(IDebugUIConstants.PREF_CONSOLE_FONT);
-        setFont(font);
-        partitioner.connect(getDocument());
-    }
-
-    /**
-     * @see org.eclipse.ui.console.AbstractConsole#init()
-     */
-    @Override
-    protected void init() {
-        JFaceResources.getFontRegistry().addListener(propertyListener);
-    }
-
-    /**
-     * @see org.eclipse.ui.console.TextConsole#dispose()
-     */
-    @Override
-    protected void dispose() {
-        JFaceResources.getFontRegistry().removeListener(propertyListener);
-        super.dispose();
-    }
-
-    /**
-     * @see org.eclipse.ui.console.TextConsole#getPartitioner()
-     */
-    @Override
-    protected IConsoleDocumentPartitioner getPartitioner() {
-        return partitioner;
-    }
-
-    /**
-     * @see org.eclipse.ui.console.TextConsole#createPage(org.eclipse.ui.console.IConsoleView)
-     */
-    @Override
-    public IPageBookViewPage createPage(IConsoleView view) {
-        return new GroovyConsolePage(this, view);
     }
 }
