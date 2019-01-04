4da961b341cb3bf38f8b2ded7be12b3b88a2b882
Eric Milles <eric.milles@thomsonreuters.com>
2019-01-04 15:30:18 -0600

    Fox for NPE

--- ide/org.codehaus.groovy.eclipse.codeassist.completion/src/org/codehaus/groovy/eclipse/codeassist/processors/PackageCompletionProcessor.java
+++ ide/org.codehaus.groovy.eclipse.codeassist.completion/src/org/codehaus/groovy/eclipse/codeassist/processors/PackageCompletionProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2018 the original author or authors.
+ * Copyright 2009-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,8 @@ public class PackageCompletionProcessor extends AbstractGroovyCompletionProcesso
             return false;
         }
         // check for parameter name completion
-        if (context.location == ContentAssistLocation.PARAMETER) {
+        if (context.location == ContentAssistLocation.PARAMETER &&
+                context.completionNode instanceof AnnotatedNode) {
             AnnotatedNode completionNode = (AnnotatedNode) context.completionNode;
             if (completionNode.getStart() < completionNode.getNameStart() &&
                     context.completionLocation >= completionNode.getNameStart()) {
