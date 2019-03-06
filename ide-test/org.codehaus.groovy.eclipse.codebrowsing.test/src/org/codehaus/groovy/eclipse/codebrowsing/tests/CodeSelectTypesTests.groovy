2b1f44b5ed25f7859b2a80ff0f4badbed99fd128
Eric Milles <eric.milles@thomsonreuters.com>
2019-03-06 13:06:30 -0600

    Fix for OOBE

--- ide-test/org.codehaus.groovy.eclipse.codebrowsing.test/src/org/codehaus/groovy/eclipse/codebrowsing/tests/CodeSelectTypesTests.groovy
+++ ide-test/org.codehaus.groovy.eclipse.codebrowsing.test/src/org/codehaus/groovy/eclipse/codebrowsing/tests/CodeSelectTypesTests.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2018 the original author or authors.
+ * Copyright 2009-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,6 +62,14 @@ final class CodeSelectTypesTests extends BrowsingTestSuite {
         String contents = 'abstract class Type extends java.lang.Number { }'
         assertCodeSelect([contents], 'lang', 'java.lang')
         assertCodeSelect([contents], 'java', 'java')
+    }
+
+    @Test
+    void testSelectSuperClass4() {
+        String another = 'class Super { }'
+        // "<T extends Type<T>>" allows methods to return this as sub-type (aka T)
+        String contents = 'abstract class Type<T extends Type<T>> extends Super {}'
+        assertCodeSelect([another, contents], 'Super')
     }
 
     @Test
--- ide/org.codehaus.groovy.eclipse.codebrowsing/src/org/codehaus/groovy/eclipse/codebrowsing/requestor/ASTNodeFinder.java
+++ ide/org.codehaus.groovy.eclipse.codebrowsing/src/org/codehaus/groovy/eclipse/codebrowsing/requestor/ASTNodeFinder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2018 the original author or authors.
+ * Copyright 2009-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -365,7 +365,7 @@ public class ASTNodeFinder extends DepthFirstVisitor {
         }
         // set offset beyond the class name and any generics
         GenericsType type = ArrayUtils.lastElement(node.getGenericsTypes());
-        int offset = (type != null ? type.getEnd() : node.getNameEnd()) + 1;
+        int offset = (type != null ? type.getEnd() : node.getNameEnd() + 1);
         String source, src = null;
         try {
             source = (src = readClassDeclaration(node)).substring(offset - node.getStart());
