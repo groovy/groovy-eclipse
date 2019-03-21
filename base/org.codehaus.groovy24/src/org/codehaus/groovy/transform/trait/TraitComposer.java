4a3cfa44f0088a7cd0769b89dac3547d86fee97c
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-21 00:05:59 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * [ ] http://www.apache.org/licenses/ with 15 occurrences migrated to:
      https://www.apache.org/licenses/ ([https](https://www.apache.org/licenses/) result 200).
    * [ ] http://www.apache.org/licenses/LICENSE with 39 occurrences migrated to:
      https://www.apache.org/licenses/LICENSE ([https](https://www.apache.org/licenses/LICENSE) result 200).
    * [ ] http://www.apache.org/licenses/LICENSE-2.0 with 1240 occurrences migrated to:
      https://www.apache.org/licenses/LICENSE-2.0 ([https](https://www.apache.org/licenses/LICENSE-2.0) result 200).
    * [ ] http://www.apache.org/licenses/LICENSE-2.0.html with 44 occurrences migrated to:
      https://www.apache.org/licenses/LICENSE-2.0.html ([https](https://www.apache.org/licenses/LICENSE-2.0.html) result 200).

--- base/org.codehaus.groovy24/src/org/codehaus/groovy/transform/trait/TraitComposer.java
+++ base/org.codehaus.groovy24/src/org/codehaus/groovy/transform/trait/TraitComposer.java
@@ -7,7 +7,7 @@
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *    https://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
@@ -83,7 +83,7 @@ import static org.codehaus.groovy.ast.tools.GenericsUtils.correctToGenericsSpecR
  * This class contains a static utility method {@link #doExtendTraits(org.codehaus.groovy.ast.ClassNode, org.codehaus.groovy.control.SourceUnit, org.codehaus.groovy.control.CompilationUnit)}
  * aimed at generating code for a classnode implementing a trait.
  *
- * @author Cédric Champeau
+ * @author Cï¿½dric Champeau
  * @since 2.3.0
  */
 public abstract class TraitComposer {
