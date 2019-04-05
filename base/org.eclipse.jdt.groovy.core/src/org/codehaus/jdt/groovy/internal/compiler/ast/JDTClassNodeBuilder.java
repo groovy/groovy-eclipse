bca9876d1c674f106b7022b656f9137ee565f190
Eric Milles <eric.milles@thomsonreuters.com>
2019-04-05 14:27:15 -0500

    Fix for #863: link raw type to base generic type (aka "List -> List<E>")

--- base/org.eclipse.jdt.groovy.core/src/org/codehaus/jdt/groovy/internal/compiler/ast/JDTClassNodeBuilder.java
+++ base/org.eclipse.jdt.groovy.core/src/org/codehaus/jdt/groovy/internal/compiler/ast/JDTClassNodeBuilder.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2009-2018 the original author or authors.
+ * Copyright 2009-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -222,7 +222,9 @@ class JDTClassNodeBuilder {
 
     private ClassNode configureParameterizedType(ParameterizedTypeBinding tb) {
         if (tb instanceof RawTypeBinding) {
-            return new JDTClassNode(tb, resolver); // doesn't need generics initializing
+            JDTClassNode cn = new JDTClassNode(tb, resolver);
+            setRedirect(cn, configureType(tb.genericType()));
+            return cn;
         }
         TypeBinding rt = toRawType(tb);
         if (rt instanceof ParameterizedTypeBinding && !(rt instanceof RawTypeBinding)) {
