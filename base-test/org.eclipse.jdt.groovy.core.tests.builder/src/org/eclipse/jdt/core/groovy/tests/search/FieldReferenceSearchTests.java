59c91275d76dbd77f6b6b0c592a5be6c64d50e8f
Eric Milles <eric.milles@thomsonreuters.com>
2019-12-30 18:46:30 -0600

    minor fix-ups

--- base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/search/FieldReferenceSearchTests.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/search/FieldReferenceSearchTests.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertEquals;
 import org.codehaus.jdt.groovy.model.GroovyCompilationUnit;
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.groovy.tests.MockPossibleMatch;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.SearchPattern;
@@ -30,79 +31,134 @@ import org.junit.Test;
 public final class FieldReferenceSearchTests extends SearchTestSuite {
 
     @Test
-    public void testFieldReferencesInScript1() throws Exception {
-        doTestForTwoFieldReferencesInScript("new First().xxx\nnew First()\n.\nxxx");
-    }
-
-    @Test
-    public void testFieldReferencesInScript2() throws Exception {
-        doTestForTwoFieldReferencesInScript("First f = new First()\n f.xxx = f.xxx");
-    }
-
-    @Test
-    public void testFieldReferencesInScript3() throws Exception {
-        doTestForTwoFieldReferencesInScript("First f = new First()\n \"$f.xxx\"\n\"$f.xxx\"");
-    }
-
-    @Test
-    public void testFieldReferencesInScript4() throws Exception {
-        doTestForTwoFieldReferencesInScriptWithQuotes("First f = new First()\n f.'xxx'\nf.'xxx'");
-    }
-
-    @Test
-    public void testFieldReferencesInScript5() throws Exception {
-        doTestForTwoFieldReferencesInScript("First f = new First()\n f.xxx\ndef xxx = 0\nxxx++\nf.xxx");
-    }
-
-    @Test
-    public void testFieldReferencesInScript6() throws Exception {
-        doTestForTwoFieldReferencesInScript("class SubClass extends First { } \n SubClass f = new SubClass()\n f.xxx\ndef xxx = 0\nxxx++\nf.xxx");
-    }
-
-    @Test
-    public void testFieldReferencesInScript7() throws Exception {
-        createUnit("Other.groovy", "class Other { def xxx }");
-        doTestForTwoFieldReferencesInScript("class SubClass extends First { } \n SubClass f = new SubClass()\n f.xxx\nnew Other().xxx = 0\nf.xxx");
-    }
-
-    @Test
-    public void testFieldReferencesInScript8() throws Exception {
+    public void testFieldReferencesInScript1() {
         doTestForTwoFieldReferencesInScript(
-            "class SubClass extends First { } \n " +
-            "def f = new SubClass()\n " +
+            "new First().xxx\n" +
+            "new First()\n" +
+            ".\n" +
+            "xxx\n");
+    }
+
+    @Test
+    public void testFieldReferencesInScript2() {
+        doTestForTwoFieldReferencesInScript(
+            "First f = new First()\n" +
+            "f.xxx = f.xxx\n");
+    }
+
+    @Test
+    public void testFieldReferencesInScript3() {
+        doTestForTwoFieldReferencesInScript(
+            "First f = new First()\n" +
+            "\"$f.xxx\"\n" +
+            "\"$f.xxx\"\n");
+    }
+
+    @Test
+    public void testFieldReferencesInScript4() {
+        doTestForTwoFieldReferencesInScriptWithQuotes(
+            "First f = new First()\n" +
+            "f.'xxx'\n" +
+            "f.'xxx'\n");
+    }
+
+    @Test
+    public void testFieldReferencesInScript5() {
+        doTestForTwoFieldReferencesInScript(
+            "First f = new First()\n" +
+            "f.xxx\n" +
+            "def xxx = 0\n" +
+            "xxx++\n" +
+            "f.xxx\n");
+    }
+
+    @Test
+    public void testFieldReferencesInScript6() {
+        doTestForTwoFieldReferencesInScript(
+            "class SubClass extends First {\n" +
+            "}\n" +
+            "SubClass f = new SubClass()\n" +
+            "f.xxx\n" +
+            "def xxx = 0\n" +
+            "xxx++\n" +
+            "f.xxx\n");
+    }
+
+    @Test
+    public void testFieldReferencesInScript7() {
+        createUnit("Other.groovy",
+            "class Other {\n" +
+            "  def xxx\n" +
+            "}\n");
+        doTestForTwoFieldReferencesInScript(
+            "class SubClass extends First {\n" +
+            "}\n" +
+            "SubClass f = new SubClass()\n" +
+            "f.xxx\n" +
+            "new Other().xxx = 0\n" +
+            "f.xxx\n");
+    }
+
+    @Test
+    public void testFieldReferencesInScript8() {
+        doTestForTwoFieldReferencesInScript(
+            "class SubClass extends First {\n" +
+            "}\n" +
+            "def f = new SubClass()\n" +
             "f.xxx\n" + // here
             "f = 9\n" +
             "f.xxx\n" +  // invalid reference
             "f = new SubClass()\n" +
-            "f.xxx");  // here
+            "f.xxx\n");  // here
     }
 
     @Test
-    public void testFieldReferencesInClass1() throws Exception {
-        doTestForTwoFieldReferencesInClass("class Second extends First { \ndef method() { this.xxx }\ndef xxx() { }\n def method2() { super.xxx }}");
+    public void testFieldReferencesInClass1() {
+        doTestForTwoFieldReferencesInClass(
+            "class Second extends First {\n" +
+            "  def method() {\n" +
+            "    this.xxx\n" +
+            "  }\n" +
+            "  def xxx() {\n" +
+            "  }\n" +
+            "  def method2() {\n" +
+            "    super.xxx\n" +
+            "  }\n" +
+            "}\n");
     }
 
     @Test
-    public void testFieldReferencesInClass2() throws Exception {
-        doTestForTwoFieldReferencesInClass("class Second extends First { \ndef method() { xxx }\ndef xxxDONT_SHADOW_SUPER_FIELD() { }\n def method2(xxx) { xxx = super.xxx }}");
+    public void testFieldReferencesInClass2() {
+        doTestForTwoFieldReferencesInClass(
+            "class Second extends First {\n" +
+            "  def method() {\n" +
+            "    xxx\n" +
+            "  }\n" +
+            "  def xxxDONT_SHADOW_SUPER_FIELD() {\n" +
+            "  }\n" +
+            "  def method2(xxx) {\n" +
+            "    xxx = super.xxx\n" +
+            "  }\n" +
+            "}\n");
     }
 
     @Test
-    public void testFieldReferencesInClass3() throws Exception {
+    public void testFieldReferencesInClass3() {
         doTestForTwoFieldReferencesInClass(
             "class Second extends First {\n" +
             "  def method() {\n" +
             "    this.xxx = 'nothing'\n" + // yes
             "  }\n" +
-            "  def xxx() { }\n" +  // no
+            "  def xxx() {\n" +  // no
+            "  }\n" +
             "  def method2() {\n" +  // no
             "    def nothing = super.xxx()\n" +  // yes...field reference used as a closure
             "  }\n" +
-            "}");
+            "}\n");
     }
 
     @Test
-    public void testFieldReferencesInClass4() throws Exception {
+    public void testFieldReferencesInClass4() {
         createUnit("Third",
             "class Third {\n" +
             "  def xxx\n" + // no
@@ -112,14 +168,14 @@ public final class FieldReferenceSearchTests extends SearchTestSuite {
             "  def method() {\n" +
             "    this.xxx = 'nothing'\n" + // yes
             "  }\n" +
-            "  def xxx() { }\n" +  // no
+            "  def xxx() {}\n" +  // no
             "  def method3(xxx) {\n" +  // no
             "    new Third().xxx\n" + // no
             "    xxx()\n" + // no
             "    xxx = xxx\n" +  // no, no
             "    def nothing = super.xxx()\n" +  // yes...field reference used as a closure
             "  }\n" +
-            "}");
+            "}\n");
     }
 
     @Test
@@ -127,8 +183,12 @@ public final class FieldReferenceSearchTests extends SearchTestSuite {
         createUnit("other", "Other",
             "class Third {\n" +
             "  private String xxx\n" +
-            "  String getXxx() { xxx }\n" +
-            "  void setXxx(String xxx) { this.xxx = xxx }\n" +
+            "  String getXxx() {\n" +
+            "    xxx\n" +
+            "  }\n" +
+            "  void setXxx(String xxx) {\n" +
+            "    this.xxx = xxx\n" +
+            "  }\n" +
             "}\n" +
             "\n" +
             "@groovy.transform.CompileStatic\n" +
@@ -153,7 +213,7 @@ public final class FieldReferenceSearchTests extends SearchTestSuite {
     }
 
     @Test // https://github.com/groovy/groovy-eclipse/issues/891
-    public void testFieldReferencesInClass6() throws Exception {
+    public void testFieldReferencesInClass6() {
         String contents =
             "@groovy.transform.CompileStatic\n" +
             "class Pogo {\n" +
@@ -181,7 +241,7 @@ public final class FieldReferenceSearchTests extends SearchTestSuite {
     }
 
     @Test // https://github.com/groovy/groovy-eclipse/issues/939
-    public void testFieldReferencesInClosure() throws Exception {
+    public void testFieldReferencesInClosure() {
         String contents =
             "class Pogo {\n" +
             "  boolean flag\n" +
@@ -211,46 +271,68 @@ public final class FieldReferenceSearchTests extends SearchTestSuite {
     }
 
     @Test
-    public void testFieldReferenceInGString1() throws Exception {
-        doTestForTwoFieldReferencesInGString("class Second extends First {\ndef x() { \"${xxx}\"\n\"${xxx.toString()}\" } }");
+    public void testFieldReferenceInGString1() {
+        doTestForTwoFieldReferencesInGString(
+            "class Second extends First {\n" +
+            "  def x() {\n" +
+            "    \"${xxx}\"\n" +
+            "    \"${xxx.toString()}\"\n" +
+            "  }\n" +
+            "}\n");
     }
 
     @Test
-    public void testFieldReferenceInGString2() throws Exception {
-        doTestForTwoFieldReferencesInGString("class Second extends First {\ndef x() { \"${ xxx }\"\n\"${ xxx .toString()}\" } }");
+    public void testFieldReferenceInGString2() {
+        doTestForTwoFieldReferencesInGString(
+            "class Second extends First {\n" +
+            "  def x() {\n" +
+            "    \"${ xxx}\"\n" +
+            "    \"${ xxx .toString()}\"\n" +
+            "  }\n" +
+            "}\n");
     }
 
     @Test
-    public void testFieldReferenceInGString3() throws Exception {
-        doTestForTwoFieldReferencesInGString("class Second extends First {\ndef x() { \"${xxx} ${xxx.toString()}\" } }");
+    public void testFieldReferenceInGString3() {
+        doTestForTwoFieldReferencesInGString(
+            "class Second extends First {\n" +
+            "  def x() {\n" +
+            "    \"${xxx} ${xxx.toString()}\"\n" +
+            "  }\n" +
+            "}\n");
     }
 
     @Test
-    public void testFieldReferenceInGString4() throws Exception {
-        doTestForTwoFieldReferencesInGString("class Second extends First {\ndef x() { \"${dunno(xxx)} ${super.xxx}\" } }");
+    public void testFieldReferenceInGString4() {
+        doTestForTwoFieldReferencesInGString(
+            "class Second extends First {\n" +
+            "  def x() {\n" +
+            "    \"${dunno(xxx)} ${super.xxx}\"\n" +
+            "  }\n" +
+            "}\n");
     }
 
     @Test
-    public void testFieldWritesInScript1() throws Exception {
+    public void testFieldWritesInScript1() {
         doTestForTwoFieldWritesInScript(
             "new First().xxx = 1\n" +
             "new First().xxx\n" +
             "def f = new First()\n" +
             "def y = f.xxx\n" +
-            "f.xxx = 8");
+            "f.xxx = 8\n");
     }
 
     @Test
-    public void testFieldReadsInScript1() throws Exception {
+    public void testFieldReadsInScript1() {
         doTestForTwoFieldReadsInScript(
             "new First().xxx\n" +
             "new First().xxx = 1\n" +
             "def f = new First()\n" +
-            "f.xxx = f.xxx");
+            "f.xxx = f.xxx\n");
     }
 
     @Test // https://github.com/groovy/groovy-eclipse/issues/788
-    public void testFindInnerClassField1() throws Exception {
+    public void testFindInnerClassField1() {
         String contents =
             "class Outer {\n" +
             "  static class Inner1 {\n" +
@@ -283,7 +365,7 @@ public final class FieldReferenceSearchTests extends SearchTestSuite {
     }
 
     @Test // https://github.com/groovy/groovy-eclipse/issues/788
-    public void testFindInnerClassField2() throws Exception {
+    public void testFindInnerClassField2() {
         String contents =
             "class Outer {\n" +
             "  static class Inner1 {\n" +
@@ -319,59 +401,63 @@ public final class FieldReferenceSearchTests extends SearchTestSuite {
 
     private static final String FIRST_CONTENTS_CLASS_FOR_FIELDS = "class First {\n  def xxx\n}";
 
-    private void doTestForTwoFieldWritesInScript(String secondContents) throws Exception {
+    private void doTestForTwoFieldWritesInScript(String secondContents) {
         doTestForTwoFieldReferences(FIRST_CONTENTS_CLASS_FOR_FIELDS, secondContents, true, 3, "xxx", IJavaSearchConstants.WRITE_ACCESSES);
     }
 
-    private void doTestForTwoFieldReadsInScript(String secondContents) throws Exception {
+    private void doTestForTwoFieldReadsInScript(String secondContents) {
         doTestForTwoFieldReferences(FIRST_CONTENTS_CLASS_FOR_FIELDS, secondContents, true, 3, "xxx", IJavaSearchConstants.READ_ACCESSES);
     }
 
-    private void doTestForTwoFieldReferencesInScript(String secondContents) throws Exception {
+    private void doTestForTwoFieldReferencesInScript(String secondContents) {
         doTestForTwoFieldReferences(FIRST_CONTENTS_CLASS_FOR_FIELDS, secondContents, true, 3, "xxx");
     }
 
-    private void doTestForTwoFieldReferencesInScriptWithQuotes(String secondContents) throws Exception {
+    private void doTestForTwoFieldReferencesInScriptWithQuotes(String secondContents) {
         doTestForTwoFieldReferences(FIRST_CONTENTS_CLASS_FOR_FIELDS, secondContents, true, 3, "'xxx'");
     }
 
-    private void doTestForTwoFieldReferencesInClass(String secondContents) throws Exception {
+    private void doTestForTwoFieldReferencesInClass(String secondContents) {
         doTestForTwoFieldReferences(FIRST_CONTENTS_CLASS_FOR_FIELDS, secondContents, false, 0, "xxx");
     }
 
-    private void doTestForTwoFieldReferences(String firstContents, String secondContents, boolean contentsIsScript, int offsetInParent, String matchName) throws Exception {
-        doTestForTwoFieldReferences(firstContents, secondContents, contentsIsScript, offsetInParent, matchName, IJavaSearchConstants.REFERENCES);
+    private void doTestForTwoFieldReferences(String firstContents, String secondContents, boolean isScript, int offsetInParent, String matchName) {
+        doTestForTwoFieldReferences(firstContents, secondContents, isScript, offsetInParent, matchName, IJavaSearchConstants.REFERENCES);
     }
 
-    private void doTestForTwoFieldReferences(String firstContents, String secondContents, boolean contentsIsScript, int offsetInParent, String matchName, int searchFlags) throws Exception {
+    private void doTestForTwoFieldReferences(String firstContents, String secondContents, boolean isScript, int offsetInParent, String matchName, int flags) {
         String firstClassName = "First";
         String secondClassName = "Second";
         String matchedFieldName = "xxx";
         GroovyCompilationUnit first = createUnit(firstClassName, firstContents);
         IField firstField = findType(firstClassName, first).getField(matchedFieldName);
-        SearchPattern pattern = SearchPattern.createPattern(firstField, searchFlags);
+        SearchPattern pattern = SearchPattern.createPattern(firstField, flags);
 
         GroovyCompilationUnit second = createUnit(secondClassName, secondContents);
-        IJavaElement firstMatchEnclosingElement;
-        IJavaElement secondMatchEnclosingElement;
-        if (contentsIsScript) {
-            firstMatchEnclosingElement = findType(secondClassName, second).getChildren()[offsetInParent];
-            secondMatchEnclosingElement = findType(secondClassName, second).getChildren()[offsetInParent];
-        } else {
-            firstMatchEnclosingElement = findType(secondClassName, second).getChildren()[offsetInParent];
-            secondMatchEnclosingElement = findType(secondClassName, second).getChildren()[offsetInParent+2];
-        }
-        // match is enclosed in run method (for script), or x method for class
+        try {
+            IJavaElement firstMatchEnclosingElement;
+            IJavaElement secondMatchEnclosingElement;
+            if (isScript) {
+                firstMatchEnclosingElement = findType(secondClassName, second).getChildren()[offsetInParent];
+                secondMatchEnclosingElement = findType(secondClassName, second).getChildren()[offsetInParent];
+            } else {
+                firstMatchEnclosingElement = findType(secondClassName, second).getChildren()[offsetInParent];
+                secondMatchEnclosingElement = findType(secondClassName, second).getChildren()[offsetInParent + 2];
+            }
+            // match is enclosed in run method (for script), or x method for class
 
-        checkMatches(secondContents, matchName, pattern, second, firstMatchEnclosingElement, secondMatchEnclosingElement);
+            checkMatches(secondContents, matchName, pattern, second, firstMatchEnclosingElement, secondMatchEnclosingElement);
+        } catch (JavaModelException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void doTestForTwoFieldReferencesInGString(String secondContents) throws Exception {
+    private void doTestForTwoFieldReferencesInGString(String secondContents) {
         doTestForTwoFieldReferencesInGString(FIRST_CONTENTS_CLASS_FOR_FIELDS, secondContents, "xxx");
     }
 
     // as above, but enclosing element is always the first child of the enclosing type
-    private void doTestForTwoFieldReferencesInGString(String firstContents, String secondContents, String matchName) throws Exception {
+    private void doTestForTwoFieldReferencesInGString(String firstContents, String secondContents, String matchName) {
         String firstClassName = "First";
         String secondClassName = "Second";
         String matchedFieldName = "xxx";
@@ -380,9 +466,12 @@ public final class FieldReferenceSearchTests extends SearchTestSuite {
         SearchPattern pattern = SearchPattern.createPattern(firstField, IJavaSearchConstants.REFERENCES);
 
         GroovyCompilationUnit second = createUnit(secondClassName, secondContents);
-        IJavaElement firstMatchEnclosingElement = findType(secondClassName, second).getChildren()[0];
-        IJavaElement secondMatchEnclosingElement = findType(secondClassName, second).getChildren()[0];
-
-        checkMatches(secondContents, matchName, pattern, second, firstMatchEnclosingElement, secondMatchEnclosingElement);
+        try {
+            IJavaElement firstMatchEnclosingElement = findType(secondClassName, second).getChildren()[0];
+            IJavaElement secondMatchEnclosingElement = findType(secondClassName, second).getChildren()[0];
+            checkMatches(secondContents, matchName, pattern, second, firstMatchEnclosingElement, secondMatchEnclosingElement);
+        } catch (JavaModelException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
