332fe965a5266e2d5eee9420ea99d83db51e4bdb
Eric Milles <eric.milles@thomsonreuters.com>
2020-01-02 11:00:00 -0600

    minor fix-ups

--- base/org.eclipse.jdt.groovy.core/src/org/eclipse/jdt/groovy/search/TypeInferencingVisitorWithRequestor.java
+++ base/org.eclipse.jdt.groovy.core/src/org/eclipse/jdt/groovy/search/TypeInferencingVisitorWithRequestor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2019 the original author or authors.
+ * Copyright 2009-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -146,7 +146,6 @@ public class TypeInferencingVisitorWithRequestor extends ClassCodeVisitorSupport
         Util.log(s);
     }
 
-    // shared instances for several method checks below
     private static final String[] NO_PARAMS = CharOperation.NO_STRINGS;
     private static final Parameter[] NO_PARAMETERS = Parameter.EMPTY_ARRAY;
 
