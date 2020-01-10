0e3b50a078fdda24801b8442bde3dbf259e41505
Eric Milles <eric.milles@thomsonreuters.com>
2020-01-10 11:45:28 -0600

    Fix for #1018: save source context in uncaught exception error message

--- base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/locations/SourceLocationsTests.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/locations/SourceLocationsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2019 the original author or authors.
+ * Copyright 2009-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -704,7 +704,7 @@ public final class SourceLocationsTests extends BuilderTestSuite {
 
     @Test // STS-3878
     public void testErrorPositionForUnsupportedOperation() throws Exception {
-        assumeTrue(!isAtLeastGroovy(26));
+        assumeTrue(!isAtLeastGroovy(30));
         String source =
             "def a = 'a'\n" +
             "def b = 'b'\n" +
