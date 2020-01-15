52a0e335d03308a7d4cd37efee71f763fff300b3
Eric Milles <eric.milles@thomsonreuters.com>
2020-01-15 14:46:08 -0600

    rework console hyperlinks

--- ide-test/org.codehaus.groovy.alltests/src/org/codehaus/groovy/alltests/AllGroovyTests.groovy
+++ ide-test/org.codehaus.groovy.alltests/src/org/codehaus/groovy/alltests/AllGroovyTests.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2019 the original author or authors.
+ * Copyright 2009-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -142,7 +142,6 @@ import org.junit.runners.Suite
     org.codehaus.groovy.eclipse.test.core.util.TokenStreamTests,
     org.codehaus.groovy.eclipse.test.debug.BreakpointLocationTests,
     org.codehaus.groovy.eclipse.test.debug.DebugBreakpointsTests,
-    org.codehaus.groovy.eclipse.test.launch.ConsoleLineTrackerTests,
     org.codehaus.groovy.eclipse.test.launch.GroovyScriptLaunchShortcutTests,
     org.codehaus.groovy.eclipse.test.search.FindOccurrencesTests,
     org.codehaus.groovy.eclipse.test.ui.BracketInserterTests,
