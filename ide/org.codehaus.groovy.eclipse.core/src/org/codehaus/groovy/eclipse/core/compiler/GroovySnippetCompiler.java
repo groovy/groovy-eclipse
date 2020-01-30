42cfb9d469fc452a3f6a822f37b36d713925ccad
Eric Milles <eric.milles@thomsonreuters.com>
2020-01-30 16:26:48 -0600

    increase target for parsing snippets from 1.2 to 1.7

--- ide/org.codehaus.groovy.eclipse.core/src/org/codehaus/groovy/eclipse/core/compiler/GroovySnippetCompiler.java
+++ ide/org.codehaus.groovy.eclipse.core/src/org/codehaus/groovy/eclipse/core/compiler/GroovySnippetCompiler.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2009-2018 the original author or authors.
+ * Copyright 2009-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,14 +18,12 @@ package org.codehaus.groovy.eclipse.core.compiler;
 import static org.eclipse.jdt.groovy.core.util.GroovyUtils.isSynthetic;
 
 import java.util.Iterator;
-import java.util.Map;
 
 import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.ast.MethodNode;
 import org.codehaus.groovy.ast.ModuleNode;
 import org.codehaus.groovy.eclipse.core.GroovyCore;
 import org.codehaus.jdt.groovy.internal.compiler.ast.GroovyCompilationUnitDeclaration;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.WorkingCopyOwner;
 import org.eclipse.jdt.core.dom.CompilationUnitResolver;
@@ -46,10 +44,12 @@ import org.eclipse.jdt.internal.core.SearchableEnvironment;
  */
 public class GroovySnippetCompiler {
 
+    private CompilerOptions compilerOptions;
     private INameEnvironment nameEnvironment;
 
-    public GroovySnippetCompiler(JavaProject project) {
+    public GroovySnippetCompiler(final JavaProject project) {
         try {
+            compilerOptions = new CompilerOptions(project.getOptions(true));
             nameEnvironment = new SearchableEnvironment(project, (WorkingCopyOwner) null, false);
         } catch (JavaModelException e) {
             GroovyCore.logException("Problem initializing snippet compiler for project " + project.getElementName(), e);
@@ -64,7 +64,7 @@ public class GroovySnippetCompiler {
      * @param source the groovy source code to compile
      * @param sourcePath the path including file name to compile; may be {@code null}
      */
-    public ModuleNode compile(String source, String sourcePath) {
+    public ModuleNode compile(final String source, final String sourcePath) {
         GroovyCompilationUnitDeclaration decl = internalCompile(source, sourcePath);
         ModuleNode node = decl.getModuleNode();
 
@@ -80,21 +80,19 @@ public class GroovySnippetCompiler {
         return node;
     }
 
-    public CompilationResult compileForErrors(String source, String sourcePath) {
+    public CompilationResult compileForErrors(final String source, final String sourcePath) {
         GroovyCompilationUnitDeclaration unit = internalCompile(source, sourcePath);
         return unit.compilationResult();
     }
 
-    private GroovyCompilationUnitDeclaration internalCompile(String source, String sourcePath) {
+    private GroovyCompilationUnitDeclaration internalCompile(final String source, String sourcePath) {
         if (sourcePath == null) {
-            sourcePath = "Nothing.groovy";
+            sourcePath = "snippet.groovy";
         } else if (!ContentTypeUtils.isGroovyLikeFileName(sourcePath)) {
             sourcePath = sourcePath.concat(".groovy");
         }
 
-        Map<String, String> options = JavaCore.getOptions();
-        options.put(CompilerOptions.OPTIONG_BuildGroovyFiles, CompilerOptions.ENABLED);
-        Compiler compiler = new CompilationUnitResolver(nameEnvironment, DefaultErrorHandlingPolicies.proceedWithAllProblems(), new CompilerOptions(options), result -> {}, new DefaultProblemFactory(), null, true);
+        Compiler compiler = new CompilationUnitResolver(nameEnvironment, DefaultErrorHandlingPolicies.proceedWithAllProblems(), compilerOptions, result -> {}, new DefaultProblemFactory(), null, true);
         GroovyCompilationUnitDeclaration decl = (GroovyCompilationUnitDeclaration) compiler.resolve(new MockCompilationUnit(source.toCharArray(), sourcePath.toCharArray()), true, false, false);
         return decl;
     }
@@ -106,7 +104,7 @@ public class GroovySnippetCompiler {
     /**
      * Compiles source code into a module node when there is no file name.
      */
-    public ModuleNode compile(String source) {
+    public ModuleNode compile(final String source) {
         return compile(source, null);
     }
 }
