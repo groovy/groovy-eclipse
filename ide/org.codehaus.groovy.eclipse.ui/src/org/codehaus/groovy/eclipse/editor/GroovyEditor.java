be30334410d729313d0cd6a79c0c45dd5eea5e31
Eric Milles <eric.milles@thomsonreuters.com>
2020-02-24 14:48:48 -0600

    move call to super.dispose()
    #1040

--- ide/org.codehaus.groovy.eclipse.ui/src/org/codehaus/groovy/eclipse/editor/GroovyEditor.java
+++ ide/org.codehaus.groovy.eclipse.ui/src/org/codehaus/groovy/eclipse/editor/GroovyEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2019 the original author or authors.
+ * Copyright 2009-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -661,12 +661,12 @@ public class GroovyEditor extends CompilationUnitEditor {
 
     @Override
     public void dispose() {
-        super.dispose();
         uninstallGroovySemanticHighlighting();
         ISourceViewer sourceViewer = getSourceViewer();
         if (sourceViewer instanceof ITextViewerExtension) {
             ((ITextViewerExtension) sourceViewer).removeVerifyKeyListener(groovyBracketInserter);
         }
+        super.dispose();
     }
 
     public int getCaretOffset() {
