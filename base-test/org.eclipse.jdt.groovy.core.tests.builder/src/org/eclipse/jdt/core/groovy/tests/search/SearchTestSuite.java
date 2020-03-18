f32ded7d8b71adf89c40276ced627069558a30de
Eric Milles <eric.milles@thomsonreuters.com>
2020-03-17 19:31:33 -0500

    add jobs to skip list

--- base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/search/SearchTestSuite.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.builder/src/org/eclipse/jdt/core/groovy/tests/search/SearchTestSuite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2019 the original author or authors.
+ * Copyright 2009-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -286,9 +286,11 @@ public abstract class SearchTestSuite extends BuilderTestSuite {
             case Job.WAITING:
                 if (!Arrays.asList(
                         "animation start",
+                        "change cursor",
                         "decoration calculation",
                         "flush cache job",
                         "open blocked dialog",
+                        "refreshing view",
                         "sending problem marker updates...",
                         "update dynamic java sources working sets",
                         "update for decoration completion",
--- ide-test/org.codehaus.groovy.eclipse.tests/src/org/codehaus/groovy/eclipse/test/SynchronizationUtils.java
+++ ide-test/org.codehaus.groovy.eclipse.tests/src/org/codehaus/groovy/eclipse/test/SynchronizationUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2019 the original author or authors.
+ * Copyright 2009-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -194,5 +194,5 @@ public class SynchronizationUtils {
         }
     }
 
-    private static final List<String> SKIP_JOBS = Arrays.asList("animation start", "decoration calculation", "flush cache job", "open blocked dialog", "sending problem marker updates...", "update for decoration completion", "update dynamic java sources working sets", "update package explorer", "update progress", "usage data event consumer");
+    private static final List<String> SKIP_JOBS = Arrays.asList("animation start", "change cursor", "decoration calculation", "flush cache job", "open blocked dialog", "refreshing view", "sending problem marker updates...", "update for decoration completion", "update dynamic java sources working sets", "update package explorer", "update progress", "usage data event consumer");
 }
