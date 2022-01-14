0757bac2b3ec36ef2bb29a3344b06a6ed4406ffa
Eric Milles <eric.milles@thomsonreuters.com>
2022-01-14 15:01:17 -0600

    reduce chance of deadlock for add nature followed immediately by remove

--- ide-test/org.codehaus.groovy.eclipse.tests/src/org/codehaus/groovy/eclipse/test/actions/GroovyNatureActionTests.groovy
+++ ide-test/org.codehaus.groovy.eclipse.tests/src/org/codehaus/groovy/eclipse/test/actions/GroovyNatureActionTests.groovy
@@ -1,11 +1,11 @@
 /*
- * Copyright 2009-2017 the original author or authors.
+ * Copyright 2009-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,6 +20,7 @@ import static org.codehaus.jdt.groovy.model.GroovyNature.GROOVY_NATURE
 import org.codehaus.groovy.eclipse.actions.AddGroovyNatureAction
 import org.codehaus.groovy.eclipse.actions.RemoveGroovyNatureAction
 import org.codehaus.groovy.eclipse.test.GroovyEclipseTestSuite
+import org.codehaus.groovy.eclipse.test.SynchronizationUtils
 import org.eclipse.jdt.core.JavaCore
 import org.eclipse.jface.viewers.IStructuredSelection
 import org.eclipse.jface.viewers.StructuredSelection
@@ -98,6 +99,9 @@ final class GroovyNatureActionTests extends GroovyEclipseTestSuite {
 
         assert hasGroovyNature() : 'testProject should have Groovy nature after testing action'
         assert hasGroovyLibraries() : 'testProject should have Groovy jars after running add nature action'
+
+        SynchronizationUtils.waitForDSLDProcessingToComplete()
+        SynchronizationUtils.waitForIndexingToComplete()
 
         removeGroovyAction.run(null)
 
