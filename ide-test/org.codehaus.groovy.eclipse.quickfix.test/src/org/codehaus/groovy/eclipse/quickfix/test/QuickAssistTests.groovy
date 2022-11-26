7f8f14a995e4e0a8ffec4dba58cf7e7c8018e660
Eric Milles <eric.milles@thomsonreuters.com>
2022-11-26 10:20:19 -0600

    Fix for #1420: add `===` and `!==` to swap operands quick assist

--- ide-test/org.codehaus.groovy.eclipse.quickfix.test/src/org/codehaus/groovy/eclipse/quickfix/test/QuickAssistTests.groovy
+++ ide-test/org.codehaus.groovy.eclipse.quickfix.test/src/org/codehaus/groovy/eclipse/quickfix/test/QuickAssistTests.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2021 the original author or authors.
+ * Copyright 2009-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -575,6 +575,21 @@ final class QuickAssistTests extends QuickFixTestSuite {
             'g  || a && v',
             'g  || v && a',
             '&&', new SwapLeftAndRightOperandsProposal())
+    }
+
+    @Test
+    void testSwapOperands9() {
+        assumeTrue(isParrotParser())
+
+        assertConversion(
+            'g  || a === v',
+            'g  || v === a',
+            '===', new SwapLeftAndRightOperandsProposal())
+
+        assertConversion(
+            'g  || a !== v',
+            'g  || v !== a',
+            '!==', new SwapLeftAndRightOperandsProposal())
     }
 
     @Test
