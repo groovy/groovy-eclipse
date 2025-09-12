2b3919ea9ef3e3c90a96fa63d728535401f010af
Eric Milles <eric.milles@thomsonreuters.com>
2025-09-12 12:07:16 -0500

    GROOVY-10687, GROOVY-11753

--- base/org.codehaus.groovy50/src/org/codehaus/groovy/control/CompilationUnit.java
+++ base/org.codehaus.groovy50/src/org/codehaus/groovy/control/CompilationUnit.java
@@ -23,12 +23,14 @@ import groovy.lang.GroovyRuntimeException;
 import groovy.transform.CompilationUnitAware;
 import org.codehaus.groovy.GroovyBugError;
 import org.codehaus.groovy.ast.AnnotationNode;
+import org.codehaus.groovy.ast.ClassCodeVisitorSupport;
 import org.codehaus.groovy.ast.ClassHelper;
 import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.ast.CompileUnit;
 import org.codehaus.groovy.ast.GroovyClassVisitor;
 import org.codehaus.groovy.ast.InnerClassNode;
 import org.codehaus.groovy.ast.ModuleNode;
+import org.codehaus.groovy.ast.expr.ClosureExpression;
 import org.codehaus.groovy.classgen.AsmClassGenerator;
 import org.codehaus.groovy.classgen.ClassCompletionVerifier;
 import org.codehaus.groovy.classgen.EnumCompletionVisitor;
@@ -777,40 +779,7 @@ public class CompilationUnit extends ProcessingUnit {
     private final IPrimaryClassNodeOperation classgen = new IPrimaryClassNodeOperation() {
         @Override
         public void call(final SourceUnit source, final GeneratorContext context, final ClassNode classNode) throws CompilationFailedException {
-            /* GRECLIPSE edit
-            new OptimizerVisitor(CompilationUnit.this).visitClass(classNode, source); // GROOVY-4272: repositioned from static import visitor
-
-            //
-            // Run the Verifier on the outer class
-            //
-            GroovyClassVisitor visitor = new Verifier();
-            try {
-                visitor.visitClass(classNode);
-            } catch (RuntimeParserException rpe) {
-                getErrorCollector().addError(new SyntaxException(rpe.getMessage(), rpe.getNode()), source);
-            }
-
-            visitor = new LabelVerifier(source);
-            visitor.visitClass(classNode);
-
-            visitor = new InstanceOfVerifier() {
-                @Override
-                protected SourceUnit getSourceUnit() {
-                    return source;
-                }
-            };
-            visitor.visitClass(classNode);
-
-            visitor = new ClassCompletionVerifier(source);
-            visitor.visitClass(classNode);
-
-            visitor = new ExtendedVerifier(source);
-            visitor.visitClass(classNode);
-
-            // because the class may be generated even if an error was found
-            // and that class may have an invalid format we fail here if needed
-            getErrorCollector().failIfErrors();
-            */
+            // GRECLIPSE add
             if (source != null && source.getErrorCollector().hasErrors()) return;
             // GRECLIPSE end
 
@@ -1087,7 +1056,7 @@ public class CompilationUnit extends ProcessingUnit {
                 } else {
                     count = getSuperClassCount(cn) + 1000;
                 }
-                if (cn.getOuterClass() == null && cn.getInnerClasses().hasNext()) {
+                if (cn.getOuterClass() == null && hasInnerClassWithClosure(cn)) {
                     count += 2000; // GROOVY-10687: nest host must follow members (with closures)
                 }
                 return count;
@@ -1095,6 +1064,40 @@ public class CompilationUnit extends ProcessingUnit {
         }
 
         return classes;
+    }
+
+    private static boolean hasInnerClassWithClosure(final ClassNode cn) {
+        for (var it = cn.getInnerClasses(); it.hasNext(); ) {
+            @SuppressWarnings("sync-override")//GRECLIPSE
+            class ClosureFound extends RuntimeException {
+                private static final long serialVersionUID = 1;
+                @Override public Throwable fillInStackTrace() {
+                    return this;
+                }
+            }
+
+            var visitor = new ClassCodeVisitorSupport() {
+                @Override
+                public SourceUnit getSourceUnit() {
+                    return cn.getModule().getContext();
+                }
+                @Override
+                public void visitClosureExpression(final ClosureExpression ce) {
+                    throw new ClosureFound();
+                }
+            };
+
+            ClassNode innerClass = it.next();
+            try {
+                visitor.visitClass(innerClass);
+            } catch (ClosureFound done) {
+                return true;
+            }
+            if (hasInnerClassWithClosure(innerClass)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void changeBugText(final GroovyBugError e, final SourceUnit context) {
