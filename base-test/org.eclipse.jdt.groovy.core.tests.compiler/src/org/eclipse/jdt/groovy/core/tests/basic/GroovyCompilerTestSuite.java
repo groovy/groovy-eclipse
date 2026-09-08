21e1e0f20385ce8f585cc349ae3cf95bd8041b72
Eric Milles <eric.milles@thomsonreuters.com>
2026-09-08 10:16:06 -0500

    Update the Groovy plugin to 5.1.2
    for #1694

--- base-test/org.eclipse.jdt.groovy.core.tests.compiler/src/org/eclipse/jdt/groovy/core/tests/basic/GroovyCompilerTestSuite.java
+++ base-test/org.eclipse.jdt.groovy.core.tests.compiler/src/org/eclipse/jdt/groovy/core/tests/basic/GroovyCompilerTestSuite.java
@@ -147,7 +147,7 @@ public abstract class GroovyCompilerTestSuite {
                 String[] cps = super.getDefaultClassPaths();
                 String[] newcps = Arrays.copyOf(cps, cps.length + 2);
 
-                String[] groovyVersions = {"6.0.0", "5.1.1", "5.0.8", "4.0.33", "3.0.25-indy"};
+                String[] groovyVersions = {"6.0.0", "5.1.2", "5.0.8", "4.0.33", "3.0.25-indy"};
                 String[] ivyVersions = {"2.6.0", "2.5.3"};
                 try {
                     URL groovyJar = null;
